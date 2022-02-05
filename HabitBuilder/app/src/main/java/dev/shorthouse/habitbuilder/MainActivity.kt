81d4f998cd6eb47eaf85305c7ff9b27018bf693a
shorthouse <shorthouse20@gmail.com>
2022-02-05 17:53:55 +0000

    Add date picker

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/MainActivity.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/MainActivity.kt
@@ -2,11 +2,16 @@ package dev.shorthouse.habitbuilder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.shorthouse.habitbuilder.databinding.ActivityMainBinding
 
 // Hosts all fragments
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/AddHabitFragment.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/AddHabitFragment.kt
@@ -1,18 +1,23 @@
 package dev.shorthouse.habitbuilder.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.datepicker.MaterialDatePicker
 import dev.shorthouse.habitbuilder.BaseApplication
-import dev.shorthouse.habitbuilder.R
+import dev.shorthouse.habitbuilder.databinding.AddHabitFragmentBinding
 import dev.shorthouse.habitbuilder.ui.viewmodel.HabitViewModel
 import dev.shorthouse.habitbuilder.ui.viewmodel.HabitViewModelFactory
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddHabitFragment : Fragment() {
+    private var _binding: AddHabitFragmentBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: HabitViewModel by activityViewModels {
         HabitViewModelFactory(
@@ -24,6 +29,34 @@ class AddHabitFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.add_habit_fragment, container, false)
+        _binding = AddHabitFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.startDateInput.setOnClickListener {
+            displayDatePicker()
+        }
+    }
+
+    private fun displayDatePicker() {
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .build()
+
+        datePicker.show(parentFragmentManager, "HABIT_DATE_PICKER")
+
+        val dateFormatter = SimpleDateFormat("EEE dd MMM yyyy", Locale.getDefault())
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            binding.startDateInput.setText(dateFormatter.format(selection))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
