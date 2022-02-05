e74c267806c311f871b9fce91be25dabeb62077d
shorthouse <shorthouse20@gmail.com>
2022-02-05 14:02:53 +0000

    Add base application

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/BaseApplication.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/BaseApplication.kt
@@ -1,4 +1,10 @@
 package dev.shorthouse.habitbuilder
 
-class BaseApplication {
+import android.app.Application
+import dev.shorthouse.habitbuilder.data.HabitDatabase
+
+class BaseApplication: Application() {
+    val database: HabitDatabase by lazy {
+        HabitDatabase.getDatabase(this)
+    }
 }
