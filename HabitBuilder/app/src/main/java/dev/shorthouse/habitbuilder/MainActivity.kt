f36ece93f4b93b84a4aa0b389edadcc44720be4e
shorthouse <shorthouse20@gmail.com>
2022-02-05 13:12:46 +0000

    Nav graph in gradle

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/MainActivity.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.shorthouse.habitbuilder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Hosts all fragments
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
