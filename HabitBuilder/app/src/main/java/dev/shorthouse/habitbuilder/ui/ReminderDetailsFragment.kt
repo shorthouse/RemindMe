04d41f6e112b6898c64eebd5241e73cba7e4dc31
shorthouse <shorthouse20@gmail.com>
2022-03-01 20:17:23 +0000

    Build up reminder details

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/ReminderDetailsFragment.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/ReminderDetailsFragment.kt
@@ -7,10 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import dev.shorthouse.habitbuilder.BaseApplication
 import dev.shorthouse.habitbuilder.R
 import dev.shorthouse.habitbuilder.databinding.FragmentAddReminderBinding
 import dev.shorthouse.habitbuilder.databinding.FragmentReminderDetailsBinding
+import dev.shorthouse.habitbuilder.model.Reminder
 import dev.shorthouse.habitbuilder.ui.viewmodel.AddReminderViewModel
 import dev.shorthouse.habitbuilder.ui.viewmodel.AddReminderViewModelFactory
 import dev.shorthouse.habitbuilder.ui.viewmodel.ReminderDetailsViewModel
@@ -19,6 +21,9 @@ import dev.shorthouse.habitbuilder.ui.viewmodel.ReminderDetailsViewModelFactory
 class ReminderDetailsFragment : Fragment() {
     private var _binding: FragmentReminderDetailsBinding? = null
     private val binding get() = _binding!!
+
+    private val navigationArgs: ReminderDetailsFragmentArgs by navArgs()
+    private lateinit var reminder: Reminder
 
     private val viewModel: ReminderDetailsViewModel by activityViewModels {
         ReminderDetailsViewModelFactory(
@@ -34,4 +39,25 @@ class ReminderDetailsFragment : Fragment() {
         _binding = FragmentReminderDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = navigationArgs.id
+
+        viewModel.getReminder(id).observe(this.viewLifecycleOwner) {
+            reminder = it
+            bindReminder()
+        }
+    }
+
+    private fun bindReminder() {
+        binding.apply {
+            name.text = reminder.name
+            startDate.text = reminder.reminderEpoch.toString()
+            startTime.text = reminder.reminderEpoch.toString()
+            notes.text = reminder.notes
+        }
+    }
+
 }
--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/viewmodel/ReminderDetailsViewModel.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/viewmodel/ReminderDetailsViewModel.kt
@@ -1,12 +1,18 @@
 package dev.shorthouse.habitbuilder.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import dev.shorthouse.habitbuilder.data.ReminderDao
+import dev.shorthouse.habitbuilder.model.Reminder
 
 class ReminderDetailsViewModel(private val reminderDao: ReminderDao
 ) : ViewModel() {
 
+    fun getReminder(id: Long): LiveData<Reminder> {
+        return reminderDao.getReminder(id).asLiveData()
+    }
 }
 
 class ReminderDetailsViewModelFactory(
