8369b18803dd2d0f6c62a5a7617f8fb115d3591b
shorthouse <shorthouse20@gmail.com>
2022-03-02 21:52:10 +0000

    Add custom toolbar

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/AddReminderFragment.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/ui/AddReminderFragment.kt
@@ -1,10 +1,7 @@
 package dev.shorthouse.habitbuilder.ui
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -32,6 +29,15 @@ class AddReminderFragment : Fragment() {
         AddReminderViewModelFactory(
             (activity?.application as BaseApplication).database.reminderDao()
         )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_reminder_app_bar, menu)
     }
 
     override fun onCreateView(
