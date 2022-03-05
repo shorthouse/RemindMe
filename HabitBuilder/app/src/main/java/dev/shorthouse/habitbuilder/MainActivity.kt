5641f055836c374d68558711fcbf1699fc5a137a
shorthouse <shorthouse20@gmail.com>
2022-03-05 19:09:00 +0000

    Fix bottom nav component so listeners not needed

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/MainActivity.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/MainActivity.kt
@@ -4,64 +4,65 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import dev.shorthouse.habitbuilder.databinding.ActivityMainBinding
-import dev.shorthouse.habitbuilder.fragments.AllReminderListFragmentDirections
-import dev.shorthouse.habitbuilder.fragments.ActiveReminderListFragmentDirections
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
+        setupNavBar()
+        setupClickListeners()
+    }
+
+    private fun setupNavBar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.active_reminders, R.id.all_reminders)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        return navHostFragment.navController
+    }
+
+    private fun setupClickListeners() {
         binding.apply {
             addReminderFab.setOnClickListener {
-                findNavController(R.id.nav_host_container).navigate(R.id.action_activeReminderListFragment_to_addReminderFragment)
-            }
-
-            bottomNavigation.setOnItemSelectedListener { item ->
-                when(item.itemId) {
-                    R.id.active_reminders -> {
-                        val action = AllReminderListFragmentDirections
-                            .actionAllReminderListFragmentToActiveReminderListFragment()
-                        navController.navigate(action)
-                        true
-                    }
-                    R.id.all_reminders -> {
-                        val action = ActiveReminderListFragmentDirections
-                            .actionActiveReminderListFragmentToAllReminderListFragment()
-                        navController.navigate(action)
-                        true
-                    }
-                    else -> false
-                }
+                navController.navigate(R.id.action_activeReminderListFragment_to_addReminderFragment)
             }
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_container)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
-
-
 }
