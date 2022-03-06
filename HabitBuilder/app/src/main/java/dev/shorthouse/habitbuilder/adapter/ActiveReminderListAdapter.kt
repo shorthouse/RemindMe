e61dd9d6c702bbd187ddff38e729b45be6f1984a
shorthouse <shorthouse20@gmail.com>
2022-03-06 16:37:22 +0000

    Remove abstract list adapter

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/adapter/ActiveReminderListAdapter.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/adapter/ActiveReminderListAdapter.kt
@@ -2,17 +2,49 @@ package dev.shorthouse.habitbuilder.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import dev.shorthouse.habitbuilder.databinding.ListItemActiveReminderBinding
 import dev.shorthouse.habitbuilder.databinding.ListItemReminderBinding
 import dev.shorthouse.habitbuilder.model.Reminder
 
 class ActiveReminderListAdapter(
     private val clickListener: (Reminder) -> Unit)
-    : AbstractReminderListAdapter(clickListener) {
+    : ListAdapter<Reminder, ActiveReminderListAdapter.ReminderViewHolder>(DiffCallback)
+{
+    class ReminderViewHolder(
+        private var binding: ListItemActiveReminderBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(reminder: Reminder) {
+            binding.reminder = reminder
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Reminder>() {
+        override fun areItemsTheSame(oldItem: Reminder, newItem: Reminder): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Reminder, newItem: Reminder): Boolean {
+            return oldItem == newItem
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReminderViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return ReminderViewHolder(
-            ListItemReminderBinding.inflate(layoutInflater, parent, false)
+            ListItemActiveReminderBinding.inflate(layoutInflater, parent, false)
         )
+    }
+
+    override fun onBindViewHolder(holder: ReminderViewHolder, position: Int) {
+        val reminder = getItem(position)
+        holder.itemView.setOnClickListener {
+            clickListener(reminder)
+        }
+        holder.bind(reminder)
     }
 }
