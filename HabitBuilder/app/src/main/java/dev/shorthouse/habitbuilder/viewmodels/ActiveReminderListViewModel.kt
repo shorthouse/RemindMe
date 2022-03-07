441ed76b61e20f89b79107539147ee5f540e34d3
shorthouse <shorthouse20@gmail.com>
2022-03-07 17:28:07 +0000

    Update viewmodel to handle repeat/one-off reminders

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/viewmodels/ActiveReminderListViewModel.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/viewmodels/ActiveReminderListViewModel.kt
@@ -1,17 +1,95 @@
 package dev.shorthouse.habitbuilder.viewmodels
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import dev.shorthouse.habitbuilder.data.ReminderDao
 import dev.shorthouse.habitbuilder.model.Reminder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.time.Instant
 
-class ActiveReminderListViewModel(private val reminderDao: ReminderDao
+class ActiveReminderListViewModel(
+    private val reminderDao: ReminderDao
 ) : ViewModel() {
-    val activeRecurringReminders = reminderDao.getActiveRecurringReminders(Instant.now().epochSecond).asLiveData()
+    val activeReminders = reminderDao.getActiveReminders(Instant.now().epochSecond).asLiveData()
+
+    fun updateDoneReminder(
+        id: Long,
+        name: String,
+        startEpoch: Long,
+        repeatInterval: Long?,
+        notes: String?,
+    ) {
+        when (repeatInterval) {
+            null -> updateDoneSingleReminder(
+                id,
+                name,
+                startEpoch,
+                repeatInterval,
+                notes,
+            )
+            else -> updateDoneRepeatReminder(
+                id,
+                name,
+                startEpoch,
+                repeatInterval,
+                notes,
+            )
+        }
+    }
+
+    private fun updateDoneSingleReminder(
+        id: Long,
+        name: String,
+        startEpoch: Long,
+        repeatInterval: Long?,
+        notes: String?,
+    ) {
+        val reminder = Reminder(
+            id = id,
+            name = name,
+            startEpoch = startEpoch,
+            repeatInterval = repeatInterval,
+            notes = notes,
+            isArchived = true,
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            reminderDao.update(reminder)
+        }
+    }
+
+    private fun updateDoneRepeatReminder(
+        id: Long,
+        name: String,
+        startEpoch: Long,
+        repeatInterval: Long,
+        notes: String?,
+    ) {
+        val reminder = Reminder(
+            id = id,
+            name = name,
+            startEpoch = getUpdatedStartEpoch(startEpoch, repeatInterval),
+            repeatInterval = repeatInterval,
+            notes = notes,
+            isArchived = false,
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            reminderDao.update(reminder)
+        }
+    }
+
+    private fun getUpdatedStartEpoch(startEpoch: Long, repeatInterval: Long): Long {
+        val timeStartEpochToNow = Instant.now().epochSecond.minus(startEpoch)
+        val numElapsedIntervals = timeStartEpochToNow.div(repeatInterval)
+        val nextInterval = numElapsedIntervals.plus(1)
+        val timeStartEpochToNextInterval = nextInterval.times(repeatInterval)
+        return startEpoch.plus(timeStartEpochToNextInterval)
+    }
+
 }
 
 class ActiveReminderListViewModelFactory(
