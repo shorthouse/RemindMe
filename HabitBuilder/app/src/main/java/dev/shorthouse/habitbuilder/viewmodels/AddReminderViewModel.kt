da23577577f6b919495fd26e6fc57f77f38a4bbb
shorthouse <shorthouse20@gmail.com>
2022-03-10 17:16:02 +0000

    Streamline date / time helpers

--- HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/viewmodels/AddReminderViewModel.kt
+++ HabitBuilder/app/src/main/java/dev/shorthouse/habitbuilder/viewmodels/AddReminderViewModel.kt
@@ -1,22 +1,20 @@
 package dev.shorthouse.habitbuilder.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.*
 import dev.shorthouse.habitbuilder.data.ReminderDao
 import dev.shorthouse.habitbuilder.model.Reminder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
 import java.time.*
 import java.time.format.DateTimeFormatter
-import java.util.*
 
-class AddReminderViewModel(private val reminderDao: ReminderDao
+class AddReminderViewModel(
+    private val reminderDao: ReminderDao
 ) : ViewModel() {
 
     companion object {
-        private val dateFormatter = DateTimeFormatter.ofPattern("EEE dd MMM yyyy")
-        private val dateTimeFormatter = DateTimeFormatter.ofPattern("EEE dd MMM yyyy HH:mm")
+        private val dateFormatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy")
+        private val dateTimeFormatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm")
 
         private const val MAX_YEARS = 10
         private const val MAX_DAYS = 364
@@ -30,7 +28,6 @@ class AddReminderViewModel(private val reminderDao: ReminderDao
         reminderInterval: Long?,
         notes: String?,
         isArchived: Boolean,
-
     ) {
         val reminder = Reminder(
             name = name,
@@ -49,7 +46,10 @@ class AddReminderViewModel(private val reminderDao: ReminderDao
         return LocalDate.parse(reminderDateText, dateFormatter)
     }
 
-    private fun convertStringToDateTime(reminderDateText: String, reminderTimeText: String): LocalDateTime {
+    private fun convertStringToDateTime(
+        reminderDateText: String,
+        reminderTimeText: String
+    ): LocalDateTime {
         val reminderDateTime = "$reminderDateText $reminderTimeText"
         return LocalDateTime.parse(reminderDateTime, dateTimeFormatter)
     }
@@ -59,8 +59,8 @@ class AddReminderViewModel(private val reminderDao: ReminderDao
         return reminderDateTime.atZone(ZoneId.systemDefault()).toEpochSecond()
     }
 
-    fun convertTimestampToDate(dateTimestamp: Long): String {
-        return Instant.ofEpochMilli(dateTimestamp)
+    fun convertInstantToDateString(instant: Instant): String {
+        return instant
             .atZone(ZoneId.systemDefault())
             .format(dateFormatter)
     }
@@ -71,12 +71,10 @@ class AddReminderViewModel(private val reminderDao: ReminderDao
                 Duration.ofHours(hours).seconds
     }
 
-    fun isDetailValid(name: String, startDate: String, reminderTime: String, ): Boolean {
+    fun isDetailValid(name: String, startDate: String, reminderTime: String): Boolean {
         return when {
             name.isBlank() -> false
-            startDate.isBlank() -> false
             convertStringToDate(startDate).isBefore(LocalDate.now()) -> false
-            reminderTime.isBlank() -> false
             convertStringToDateTime(startDate, reminderTime).isBefore(LocalDateTime.now()) -> false
             else -> true
         }
@@ -85,9 +83,7 @@ class AddReminderViewModel(private val reminderDao: ReminderDao
     fun getDetailError(name: String, startDate: String, reminderTime: String): String {
         return when {
             name.isBlank() -> "The name cannot be empty."
-            startDate.isBlank() -> "The start date cannot be empty."
             convertStringToDate(startDate).isBefore(LocalDate.now()) -> "The start date cannot be in the past."
-            reminderTime.isBlank() -> "The start time cannot be empty."
             convertStringToDateTime(startDate, reminderTime).isBefore(LocalDateTime.now()) ->
                 "The start time cannot be in the past."
             else -> ""
@@ -113,6 +109,14 @@ class AddReminderViewModel(private val reminderDao: ReminderDao
             years == 0L && days == 0L && hours == 0L -> "The interval must have a time period."
             else -> ""
         }
+    }
+
+    fun getFormattedTimeNextHour(): String {
+        val now = Instant.now()
+        val zoneId = ZoneId.systemDefault()
+        val localTime = LocalDateTime.ofInstant(now, zoneId).toLocalTime()
+        val nextHour = localTime.hour.plus(1)
+        return "${nextHour}:00"
     }
 }
 
