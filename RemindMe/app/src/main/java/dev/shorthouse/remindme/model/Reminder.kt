e80c85a7290653665e966aea105890b4f3b41129
shorthouse <shorthouse20@gmail.com>
2022-03-22 22:18:14 +0000

    Add radio button time interval

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/model/Reminder.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/model/Reminder.kt
@@ -3,7 +3,6 @@ package dev.shorthouse.remindme.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.ZonedDateTime
-import java.time.temporal.ChronoUnit
 
 @Entity
 data class Reminder(
@@ -14,8 +13,7 @@ data class Reminder(
 
     val startDateTime: ZonedDateTime,
 
-    val repeatInterval: Pair<Long, ChronoUnit>,
-    //val repeatInterval: Long?,
+    val repeatInterval: Long?,
 
     val notes: String?,
 
