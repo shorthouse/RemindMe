f825c35a8cc3b1807163eaf218ecb06fc5015443
shorthouse <shorthouse20@gmail.com>
2022-05-17 20:48:17 +0100

    Create reboot set alarm service

---
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receivers/BootCompleteReceiver.kt
@@ -0,0 +1,67 @@
+package dev.shorthouse.remindme.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
+import dev.shorthouse.remindme.data.ReminderDatabase
+import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.utilities.AlarmHelper
+import dev.shorthouse.remindme.utilities.DAYS_IN_WEEK
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+
+class BootCompleteReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == "android.intent.action.BOOT_COMPLETED" && context != null) {
+            val alarmHelper = AlarmHelper()
+
+            val remindersLiveData =
+                ReminderDatabase.getDatabase(context)
+                    .reminderDao()
+                    .getAllNonArchivedReminders()
+                    .asLiveData()
+
+            remindersLiveData.observeForever(object : Observer<List<Reminder>> {
+                override fun onChanged(reminders: List<Reminder>?) {
+
+
+                    remindersLiveData.removeObserver(this)
+                }
+            })
+
+            remindersLiveData.observeForever { reminders ->
+                val notifyingReminders = reminders.filter { it.isNotificationSent }
+
+                notifyingReminders.forEach { reminder ->
+                    alarmHelper.setNotificationAlarm(
+                        context,
+                        reminder,
+                        getTriggerAtMillis(reminder),
+                        getRepeatIntervalMillis(reminder)
+                    )
+                }
+
+                remindersLiveData.removeObserver(it)
+
+            }
+
+        }
+    }
+
+    private fun getTriggerAtMillis(reminder: Reminder): Long {
+        return reminder.startDateTime.toInstant().toEpochMilli()
+    }
+
+    private fun getRepeatIntervalMillis(reminder: Reminder): Long? {
+        val repeatInterval = reminder.repeatInterval ?: return null
+
+        val repeatIntervalDays = when (repeatInterval.timeUnit) {
+            ChronoUnit.DAYS -> repeatInterval.timeValue
+            else -> repeatInterval.timeValue * DAYS_IN_WEEK
+        }
+
+        return Duration.ofDays(repeatIntervalDays).toMillis()
+    }
+}
