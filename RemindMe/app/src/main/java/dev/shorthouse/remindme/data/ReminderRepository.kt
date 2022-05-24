8f0b0af64026d01aa563c114cf34a21d79fc70a8
shorthouse <shorthouse20@gmail.com>
2022-05-24 19:21:18 +0100

    Add repository functions

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/data/ReminderRepository.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/data/ReminderRepository.kt
@@ -1,4 +1,29 @@
 package dev.shorthouse.remindme.data
 
-class ReminderRepository {
+import dev.shorthouse.remindme.model.Reminder
+import java.time.ZonedDateTime
+
+class ReminderRepository(
+    private val reminderDao: ReminderDao
+) {
+    fun getAllReminders() = reminderDao.getReminders()
+
+    fun getNonArchivedReminders() = reminderDao.getNonArchivedReminders()
+
+    fun getActiveNonArchivedReminders(now: ZonedDateTime) =
+        reminderDao.getActiveNonArchivedReminders(now)
+
+    fun getReminder(reminderId: Long) = reminderDao.getReminder(reminderId)
+
+    suspend fun insertReminder(reminder: Reminder): Long {
+        return reminderDao.insert(reminder)
+    }
+
+    suspend fun updateReminder(reminder: Reminder) {
+        reminderDao.update(reminder)
+    }
+
+    suspend fun deleteReminder(reminder: Reminder) {
+        reminderDao.delete(reminder)
+    }
 }
