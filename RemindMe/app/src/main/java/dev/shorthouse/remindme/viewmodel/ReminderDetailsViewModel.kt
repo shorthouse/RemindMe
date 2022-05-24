a8d817a951325dd6bfc65795a5e157eb5f51b226
shorthouse <shorthouse20@gmail.com>
2022-05-24 19:21:57 +0100

    Add repo to reminder details

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/ReminderDetailsViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/ReminderDetailsViewModel.kt
@@ -2,35 +2,37 @@ package dev.shorthouse.remindme.viewmodel
 
 import androidx.lifecycle.*
 import dev.shorthouse.remindme.BaseApplication
-import dev.shorthouse.remindme.data.ReminderDao
+import dev.shorthouse.remindme.data.ReminderDatabase
+import dev.shorthouse.remindme.data.ReminderRepository
 import dev.shorthouse.remindme.model.Reminder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ReminderDetailsViewModel(
     application: BaseApplication,
-    private val reminderDao: ReminderDao
 ) : ViewModel() {
+    private val repository = ReminderRepository(
+        ReminderDatabase.getDatabase(application).reminderDao()
+    )
 
     fun getReminder(id: Long): LiveData<Reminder> {
-        return reminderDao.getReminder(id).asLiveData()
+        return repository.getReminder(id).asLiveData()
     }
 
     fun deleteReminder(reminder: Reminder) {
         viewModelScope.launch(Dispatchers.IO) {
-            reminderDao.delete(reminder)
+            repository.deleteReminder(reminder)
         }
     }
 }
 
 class ReminderDetailsViewModelFactory(
     private val application: BaseApplication,
-    private val reminderDao: ReminderDao
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ReminderDetailsViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return ReminderDetailsViewModel(application, reminderDao) as T
+            return ReminderDetailsViewModel(application) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
