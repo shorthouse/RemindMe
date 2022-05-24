ca6732e1f3ec96c6bec1b4aacc57b92e2e035e23
shorthouse <shorthouse20@gmail.com>
2022-05-24 23:24:58 +0100

    Refactor to use string values

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/receivers/TimeZoneChangedReceiver.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receivers/TimeZoneChangedReceiver.kt
@@ -3,6 +3,7 @@ package dev.shorthouse.remindme.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.services.UpdateReminderTimeZoneService
 import dev.shorthouse.remindme.utilities.UPDATED_TIME_ZONE_EXTRA
 
@@ -10,11 +11,9 @@ class TimeZoneChangedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == "android.intent.action.TIMEZONE_CHANGED" && context != null) {
             val serviceIntent = Intent(context, UpdateReminderTimeZoneService::class.java)
+            val newTimeZone = intent.getStringExtra(UPDATED_TIME_ZONE_EXTRA)
 
-            serviceIntent.putExtra(
-                UPDATED_TIME_ZONE_EXTRA,
-                intent.getStringExtra(UPDATED_TIME_ZONE_EXTRA)
-            )
+            serviceIntent.putExtra(context.getString(R.string.intent_key_timeZone), newTimeZone)
             context.startForegroundService(serviceIntent)
         }
     }
