66298bf2e47c1ca43236281a1dd6994e3e81abae
shorthouse <shorthouse20@gmail.com>
2022-05-25 21:07:43 +0100

    Remove app: prefix from binding adapters

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/BindingAdapters.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/BindingAdapters.kt
@@ -1,46 +1,36 @@
 package dev.shorthouse.remindme.utilities
 
+import android.util.Log
 import android.view.View
 import android.widget.AutoCompleteTextView
 import androidx.databinding.BindingAdapter
 import com.google.android.material.textview.MaterialTextView
 import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.data.RepeatInterval
-import java.time.Instant
-import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
 
-@BindingAdapter("app:showIfRepeatReminder")
+@BindingAdapter("showIfRepeatReminder")
 fun showIfRepeatReminder(view: View, repeatInterval: RepeatInterval?) {
     view.visibility = if (repeatInterval != null) View.VISIBLE else View.GONE
 }
 
-@BindingAdapter("app:showIfNotesExist")
+@BindingAdapter("showIfNotesExist")
 fun showIfNotesExist(view: View, notes: String?) {
     view.visibility = if (notes != null) View.VISIBLE else View.GONE
 }
 
-@BindingAdapter("app:showIfRepeatChecked")
+@BindingAdapter("showIfRepeatChecked")
 fun showIfRepeatChecked(view: View, isRepeatChecked: Boolean) {
+    Log.d("HDS", "is checked: $isRepeatChecked")
     view.visibility = if (isRepeatChecked) View.VISIBLE else View.GONE
 }
 
-@BindingAdapter("app:showIfNotificationEnabled")
+@BindingAdapter("showIfNotificationEnabled")
 fun showIfNotificationEnabled(view: View, isNotificationEnabled: Boolean) {
     view.visibility = if (isNotificationEnabled) View.VISIBLE else View.GONE
 }
 
-@BindingAdapter("reminderStartDateTime", "reminderRepeatInterval")
-fun elapsedIntervals(view: MaterialTextView, startDateTime: ZonedDateTime, repeatInterval: Long?) {
-    if (repeatInterval == null) return
-
-    val timeStartEpochToNow = Instant.now().epochSecond.minus(startDateTime.toEpochSecond())
-    val numElapsedIntervals = timeStartEpochToNow.div(repeatInterval).plus(1)
-
-    if (numElapsedIntervals > 1) view.text = numElapsedIntervals.toString()
-}
-
-@BindingAdapter("app:formattedRepeatInterval")
+@BindingAdapter("formattedRepeatInterval")
 fun formatRepeatInterval(view: MaterialTextView, repeatInterval: RepeatInterval?) {
     if (repeatInterval == null) return
 
@@ -54,7 +44,7 @@ fun formatRepeatInterval(view: MaterialTextView, repeatInterval: RepeatInterval?
     }
 }
 
-@BindingAdapter("app:dropDownText")
+@BindingAdapter("dropDownText")
 fun dropDownText(view: AutoCompleteTextView, repeatInterval: RepeatInterval?) {
     if (repeatInterval == null) {
         view.setText(view.resources.getQuantityString(R.plurals.dropdown_days, 1, 1), false)
