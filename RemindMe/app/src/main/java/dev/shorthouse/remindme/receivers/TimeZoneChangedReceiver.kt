d09ac12fd99f204a6dde0fe91278792d5662f024
shorthouse <shorthouse20@gmail.com>
2022-05-25 21:11:42 +0100

    Pull intent value name from string constants

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/receivers/TimeZoneChangedReceiver.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receivers/TimeZoneChangedReceiver.kt
@@ -5,15 +5,14 @@ import android.content.Context
 import android.content.Intent
 import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.services.UpdateReminderTimeZoneService
-import dev.shorthouse.remindme.utilities.UPDATED_TIME_ZONE_EXTRA
 
 class TimeZoneChangedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == "android.intent.action.TIMEZONE_CHANGED" && context != null) {
+            val newTimeZone = intent.getStringExtra(context.getString(R.string.intent_key_timeZone))
             val serviceIntent = Intent(context, UpdateReminderTimeZoneService::class.java)
-            val newTimeZone = intent.getStringExtra(UPDATED_TIME_ZONE_EXTRA)
-
             serviceIntent.putExtra(context.getString(R.string.intent_key_timeZone), newTimeZone)
+
             context.startForegroundService(serviceIntent)
         }
     }
