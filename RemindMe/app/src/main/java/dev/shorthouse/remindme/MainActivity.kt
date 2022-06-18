aa3efbdfba1390fe9e21d9957a60010330023e10
shorthouse <shorthouse20@gmail.com>
2022-06-18 15:52:32 +0100

    Scrim working as intended

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/MainActivity.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/MainActivity.kt
@@ -38,4 +38,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+
 }
