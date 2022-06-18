86e7200eb7341e615412e2ae7653d6f6a1aa6a98
shorthouse <shorthouse20@gmail.com>
2022-06-18 16:22:45 +0100

    Push scrim logic into viewmodel

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/ActiveReminderListFragment.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/ActiveReminderListFragment.kt
@@ -1,20 +1,14 @@
 package dev.shorthouse.remindme.fragments
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.graphics.blue
-import androidx.core.graphics.green
-import androidx.core.graphics.red
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.math.MathUtils
 import dagger.hilt.android.AndroidEntryPoint
 import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.adapter.ActiveReminderListAdapter
@@ -67,7 +61,6 @@ class ActiveReminderListFragment : Fragment() {
     }
 
     private fun setupBottomNavigationDrawer() {
-
         binding.apply {
             val bottomSheetBehavior = BottomSheetBehavior.from(navigationView)
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
@@ -89,15 +82,7 @@ class ActiveReminderListFragment : Fragment() {
             bottomSheetBehavior.addBottomSheetCallback(object :
                 BottomSheetBehavior.BottomSheetCallback() {
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                    val baseColor = Color.BLACK
-                    // 60% opacity
-                    val baseAlpha =
-                        ResourcesCompat.getFloat(resources, R.dimen.material_emphasis_medium)
-                    // Map slideOffset from [-1.0, 1.0] to [0.0, 1.0]
-                    val offset = (slideOffset - (-1f)) / (1f - (-1f)) * (1f - 0f) + 0f
-                    val alpha = MathUtils.lerp(0f, 255f, offset * baseAlpha).toInt()
-                    val color = Color.argb(alpha, baseColor.red, baseColor.green, baseColor.blue)
-                    scrim.setBackgroundColor(color)
+                    scrim.setBackgroundColor(viewModel.getScrimBackgroundColour(slideOffset))
                 }
 
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
