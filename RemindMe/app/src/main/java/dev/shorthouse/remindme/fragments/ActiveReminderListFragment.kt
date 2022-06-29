7acac9f1c2ca778afffb7e638c5fed003c539060
shorthouse <shorthouse20@gmail.com>
2022-06-29 17:55:35 +0100

    Add override back button behaviour for bottom sheet

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/ActiveReminderListFragment.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/ActiveReminderListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -79,6 +80,13 @@ class ActiveReminderListFragment : Fragment() {
                 true
             }
 
+            val backButtonCallback =
+                requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+                    if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                    }
+                }
+
             bottomSheetBehavior.addBottomSheetCallback(object :
                 BottomSheetBehavior.BottomSheetCallback() {
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
@@ -91,12 +99,15 @@ class ActiveReminderListFragment : Fragment() {
                             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
                         }
                         scrim.bringToFront()
+                        backButtonCallback.isEnabled = true
                     } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                         scrim.setOnClickListener(null)
                         activeReminderRecycler.bringToFront()
+                        backButtonCallback.isEnabled = false
                     }
                 }
             })
+
         }
     }
 
--- RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/AllReminderListFragment.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/AllReminderListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -79,6 +80,13 @@ class AllReminderListFragment : Fragment() {
                 true
             }
 
+            val backButtonCallback =
+                requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+                    if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                    }
+                }
+
             bottomSheetBehavior.addBottomSheetCallback(object :
                 BottomSheetBehavior.BottomSheetCallback() {
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
@@ -91,9 +99,11 @@ class AllReminderListFragment : Fragment() {
                             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
                         }
                         scrim.bringToFront()
+                        backButtonCallback.isEnabled = true
                     } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                         scrim.setOnClickListener(null)
                         allReminderRecycler.bringToFront()
+                        backButtonCallback.isEnabled = false
                     }
                 }
             })
