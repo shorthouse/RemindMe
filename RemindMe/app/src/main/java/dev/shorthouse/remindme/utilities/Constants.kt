84eea04dc636cc401026c9d5bf8af93010a4e9a2
shorthouse <shorthouse20@gmail.com>
2022-07-05 16:40:54 +0100

    Add new constant for scrolling

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
@@ -6,3 +6,7 @@ const val ONE_INTERVAL = 1
 const val DATE_PATTERN = "dd MMM yyyy"
 const val DATE_INPUT_PATTERN = "EEE, dd MMM yyyy"
 const val DATE_TIME_INPUT_PATTERN = "EEE, dd MMM yyyy HH:mm"
+
+const val STATE_ACTIVE_REMINDER_LIST = 0
+const val STATE_ALL_REMINDER_LIST = 1
+
