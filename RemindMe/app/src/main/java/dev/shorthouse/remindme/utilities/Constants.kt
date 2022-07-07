0e461d2c8f3862c649fcd2f016a44637d8394ec3
shorthouse <shorthouse20@gmail.com>
2022-07-07 22:33:56 +0100

    Change constant name

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
@@ -7,6 +7,7 @@ const val DATE_PATTERN = "dd MMM yyyy"
 const val DATE_INPUT_PATTERN = "EEE, dd MMM yyyy"
 const val DATE_TIME_INPUT_PATTERN = "EEE, dd MMM yyyy HH:mm"
 
-const val STATE_ACTIVE_REMINDER_LIST = 0
-const val STATE_ALL_REMINDER_LIST = 1
+const val FILTER_ACTIVE_REMINDER_LIST = 0
+const val FILTER_ALL_REMINDER_LIST = 1
+
 
