76ad3b6e28e107c550ff2a199a590fc0849f2a2b
shorthouse <shorthouse20@gmail.com>
2022-07-10 16:09:51 +0100

    Add shared view model with view pager

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/ReminderListFragment.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/fragments/ReminderListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -24,7 +25,7 @@ import dev.shorthouse.remindme.viewmodel.ReminderListViewModel
 class ReminderListFragment(private val filter: RemindersFilter) : Fragment() {
     private lateinit var binding: FragmentReminderListBinding
 
-    private val viewModel: ReminderListViewModel by viewModels()
+    private val viewModel: ReminderListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,7 +41,6 @@ class ReminderListFragment(private val filter: RemindersFilter) : Fragment() {
 
         setReminderFilter()
         setupReminderListObserver()
-        setupBottomDrawerSort()
     }
 
     private fun setReminderFilter() {
@@ -59,78 +59,5 @@ class ReminderListFragment(private val filter: RemindersFilter) : Fragment() {
         }
     }
 
-    private fun setupBottomDrawerSort() {
-        //TODO break this down into functions
-        binding.apply {
-            val bottomSheetSort = BottomSheetBehavior.from(navigationViewListSort)
-            bottomSheetSort.hide()
 
-            //TODO Map current sort to an id
-            val selectedMenuItem = when (viewModel.currentSort) {
-                RemindersSort.NEWEST_FIRST -> R.id.drawer_sort_newest_first
-                else -> R.id.drawer_sort_oldest_first
-            }
-            navigationViewListSort.setCheckedItem(selectedMenuItem)
-
-//            bottomAppBar.setOnMenuItemClickListener {
-//                bottomSheetSort.show()
-//                true
-//            }
-
-            navigationViewListSort.setNavigationItemSelectedListener { menuItem ->
-                bottomSheetSort.hide()
-
-                if (menuItem.itemId == navigationViewListSort.checkedItem?.itemId) {
-                    return@setNavigationItemSelectedListener true
-                }
-
-                //TODO map item id to sort enum
-                val sortOrder = when (menuItem.itemId) {
-                    R.id.drawer_sort_newest_first -> RemindersSort.NEWEST_FIRST
-                    else -> RemindersSort.OLDEST_FIRST
-                }
-
-                viewModel.sortReminderList(sortOrder)
-                navigationViewListSort.setCheckedItem(menuItem.itemId)
-
-                true
-            }
-
-
-            //TODO add extension functions for isShown or isHidden
-            val backButtonCallback =
-                requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-                    if (bottomSheetSort.state == BottomSheetBehavior.STATE_EXPANDED) {
-                        bottomSheetSort.hide()
-                    }
-                }
-
-            bottomSheetSort.addBottomSheetCallback(object :
-                BottomSheetBehavior.BottomSheetCallback() {
-                override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                    scrim.setBackgroundColor(viewModel.getScrimBackgroundColour(slideOffset))
-                }
-
-                override fun onStateChanged(bottomSheet: View, newState: Int) {
-                    if (newState == BottomSheetBehavior.STATE_EXPANDED) {
-                        scrim.setOnClickListener { bottomSheetSort.hide() }
-                        scrim.bringToFront()
-                        backButtonCallback.isEnabled = true
-                    } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
-                        scrim.setOnClickListener(null)
-                        reminderRecycler.bringToFront()
-                        backButtonCallback.isEnabled = false
-                    }
-                }
-            })
-        }
-    }
-
-    fun BottomSheetBehavior<NavigationView>.hide() {
-        this.state = BottomSheetBehavior.STATE_HIDDEN
-    }
-
-    fun BottomSheetBehavior<NavigationView>.show() {
-        this.state = BottomSheetBehavior.STATE_EXPANDED
-    }
 }
