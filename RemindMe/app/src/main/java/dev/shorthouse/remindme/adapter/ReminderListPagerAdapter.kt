8abe48c5ac4d61bedfd5e6ea460c00957f5f45c5
shorthouse <shorthouse20@gmail.com>
2022-07-10 14:35:54 +0100

    Add view pager adapter

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/adapter/ReminderListPagerAdapter.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/adapter/ReminderListPagerAdapter.kt
@@ -1,4 +1,22 @@
 package dev.shorthouse.remindme.adapter
 
-class ReminderListPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import dev.shorthouse.remindme.fragments.ReminderListFragment
+import dev.shorthouse.remindme.utilities.RemindersFilter
+
+class ReminderListPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    private val tabFragmentsCreators: Map<RemindersFilter, () -> Fragment> = mapOf(
+        RemindersFilter.ACTIVE_REMINDERS to { ReminderListFragment(RemindersFilter.ACTIVE_REMINDERS) },
+        RemindersFilter.ALL_REMINDERS to { ReminderListFragment(RemindersFilter.ALL_REMINDERS) }
+    )
+
+    override fun getItemCount(): Int {
+        return tabFragmentsCreators.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return tabFragmentsCreators[RemindersFilter.values()[position]]?.invoke()
+            ?: throw IndexOutOfBoundsException()
+    }
 }
