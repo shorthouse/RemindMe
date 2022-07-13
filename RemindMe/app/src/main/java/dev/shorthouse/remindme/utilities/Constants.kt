fec5256c4b1ef2ecbda2d7adcce8112b3a7b0dfd
shorthouse <shorthouse20@gmail.com>
2022-07-13 18:47:10 +0100

    Remove unused constants

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
@@ -7,10 +7,4 @@ const val DATE_PATTERN = "dd MMM yyyy"
 const val DATE_INPUT_PATTERN = "EEE, dd MMM yyyy"
 const val DATE_TIME_INPUT_PATTERN = "EEE, dd MMM yyyy HH:mm"
 
-const val FILTER_ACTIVE_REMINDER_LIST = 0
-const val FILTER_ALL_REMINDER_LIST = 1
-
-const val SORT_NEWEST_FIRST = 0
-const val SORT_OLDEST_FIRST = 1
-
 
