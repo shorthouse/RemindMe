3fcf29cd4317347368f9d4b32bb7d60dc6d22974
shorthouse <shorthouse20@gmail.com>
2022-07-18 18:45:41 +0100

    Remove unused binding adapter

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/BindingAdapters.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/BindingAdapters.kt
@@ -2,10 +2,7 @@ package dev.shorthouse.remindme.utilities
 
 import android.view.View
 import androidx.databinding.BindingAdapter
-import com.google.android.material.textview.MaterialTextView
-import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.data.RepeatInterval
-import java.time.temporal.ChronoUnit
 
 @BindingAdapter("showIfRepeatReminder")
 fun showIfRepeatReminder(view: View, repeatInterval: RepeatInterval?) {
@@ -25,18 +22,4 @@ fun showIfRepeatChecked(view: View, isRepeatChecked: Boolean) {
 @BindingAdapter("showIfNotificationEnabled")
 fun showIfNotificationEnabled(view: View, isNotificationEnabled: Boolean) {
     view.visibility = if (isNotificationEnabled) View.VISIBLE else View.GONE
-}
-
-@BindingAdapter("formattedRepeatInterval")
-fun formatRepeatInterval(view: MaterialTextView, repeatInterval: RepeatInterval?) {
-    if (repeatInterval == null) return
-
-    val timeValue = repeatInterval.timeValue.toInt()
-
-    when (repeatInterval.timeUnit) {
-        ChronoUnit.DAYS -> view.text =
-            view.resources.getQuantityString(R.plurals.interval_days, timeValue, timeValue)
-        else -> view.text =
-            view.resources.getQuantityString(R.plurals.interval_weeks, timeValue, timeValue)
-    }
 }
