9220e3255fcc177514e9b97a620f9749bcc103c5
shorthouse <shorthouse20@gmail.com>
2022-09-27 19:28:11 +0100

    Add throttle click listener

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Extensions.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Extensions.kt
@@ -1,5 +1,7 @@
 package dev.shorthouse.remindme.utilities
 
+import android.os.SystemClock
+import android.view.View
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.navigation.NavigationView
 
@@ -14,3 +16,19 @@ fun BottomSheetBehavior<NavigationView>.show() {
 fun BottomSheetBehavior<NavigationView>.isShown(): Boolean {
     return this.state == BottomSheetBehavior.STATE_EXPANDED
 }
+
+fun View.setOnClickThrottleListener(throttleDuration: Long = 1000L, action: () -> Unit) {
+    this.setOnClickListener(object : View.OnClickListener {
+        private var lastClickTime: Long = 0L
+
+        override fun onClick(view: View) {
+            val timeSinceLastClick = SystemClock.elapsedRealtime() - lastClickTime
+
+            if (timeSinceLastClick < throttleDuration) return
+            else action()
+
+            lastClickTime = SystemClock.elapsedRealtime()
+        }
+    })
+}
+
