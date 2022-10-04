0796af1aef2dd75d774cb4dc9c7a545b1e6de677
Harry <shorthouse20@gmail.com>
2022-10-04 18:46:15 +0100

    Whitespace formatting

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/CustomTestRunner.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/CustomTestRunner.kt
@@ -7,7 +7,6 @@ import dagger.hilt.android.testing.HiltTestApplication
 
 // A custom runner to set up the instrumented application class for tests.
 class CustomTestRunner : AndroidJUnitRunner() {
-
     override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
         return super.newApplication(cl, HiltTestApplication::class.java.name, context)
     }
