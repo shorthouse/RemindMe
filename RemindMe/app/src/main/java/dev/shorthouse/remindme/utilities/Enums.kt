32d9357de3cc4e5a0c2f3fae85636dffa1a9ad35
shorthouse <shorthouse20@gmail.com>
2022-10-05 19:10:22 +0100

    Add reminders list enum

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Enums.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Enums.kt
@@ -3,3 +3,7 @@ package dev.shorthouse.remindme.utilities
 enum class RemindersSort {
     EARLIEST_DATE_FIRST, LATEST_DATE_FIRST
 }
+
+enum class RemindersList {
+    ACTIVE_REMINDERS, ALL_REMINDERS
+}
