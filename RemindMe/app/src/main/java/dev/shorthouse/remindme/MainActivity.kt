d07ce3676864dca42a4a496851fdffe1fbe96f55
shorthouse <shorthouse20@gmail.com>
2022-10-08 21:24:38 +0100

    Add splash screen

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/MainActivity.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.shorthouse.remindme
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.shorthouse.remindme.databinding.ActivityMainBinding
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
