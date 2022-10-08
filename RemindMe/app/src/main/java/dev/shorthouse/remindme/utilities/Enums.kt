790cb9f01f2e296250c267945780c38750760e34
shorthouse <shorthouse20@gmail.com>
2022-10-08 21:10:36 +0100

    Remove unused RemindersList enum

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Enums.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Enums.kt
@@ -3,7 +3,3 @@ package dev.shorthouse.remindme.utilities
 enum class RemindersSort {
     EARLIEST_DATE_FIRST, LATEST_DATE_FIRST
 }
-
-enum class RemindersList {
-    ACTIVE_REMINDERS, ALL_REMINDERS
-}
