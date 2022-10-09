2a5ba9b05d95e00976d7b6633a962701b6c0f0c0
shorthouse <shorthouse20@gmail.com>
2022-10-09 21:47:27 +0100

    Remove wildcard import

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/data/source/local/ReminderDao.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/data/source/local/ReminderDao.kt
@@ -1,6 +1,11 @@
 package dev.shorthouse.remindme.data.source.local
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import dev.shorthouse.remindme.model.Reminder
 import kotlinx.coroutines.flow.Flow
 import java.time.ZonedDateTime
@@ -19,6 +24,9 @@ interface ReminderDao {
     @Query("SELECT * FROM reminder WHERE isArchived = 0")
     fun getNonArchivedReminders(): Flow<List<Reminder>>
 
+    @Query("UPDATE reminder SET isArchived = 1 WHERE id = :id")
+    fun archiveReminder(id: Long)
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(reminder: Reminder): Long
 
@@ -27,7 +35,4 @@ interface ReminderDao {
 
     @Delete
     fun delete(reminder: Reminder)
-
-    @Query("UPDATE reminder SET isArchived = 1 WHERE id = :id")
-    fun archive(id: Long)
 }
