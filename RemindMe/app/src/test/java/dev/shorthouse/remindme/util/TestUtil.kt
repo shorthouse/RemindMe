9564615b68fd6564144758f3b448e6370ac698a8
shorthouse <shorthouse20@gmail.com>
2022-10-13 22:12:28 +0100

    Add test util for creating reminders

---
+++ RemindMe/app/src/test/java/dev/shorthouse/remindme/util/TestUtil.kt
@@ -0,0 +1,27 @@
+package dev.shorthouse.remindme.util
+
+import dev.shorthouse.remindme.data.RepeatInterval
+import dev.shorthouse.remindme.model.Reminder
+import java.time.ZonedDateTime
+
+object TestUtil {
+    fun createReminder(
+        id: Long = 1L,
+        name: String = "test reminder",
+        startDateTime: ZonedDateTime = ZonedDateTime.now(),
+        repeatInterval: RepeatInterval? = null,
+        notes: String? = null,
+        isArchived: Boolean = false,
+        isNotificationSent: Boolean = false
+    ): Reminder {
+        return Reminder(
+            id = id,
+            name = name,
+            startDateTime = startDateTime,
+            repeatInterval = repeatInterval,
+            notes = notes,
+            isArchived = isArchived,
+            isNotificationSent = isNotificationSent
+        )
+    }
+}
