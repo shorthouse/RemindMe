7f5e68c5af95ac391031b6a44407c11b8527e485
shorthouse <shorthouse20@gmail.com>
2022-10-13 22:11:14 +0100

    Add tests for toolbar title

--- RemindMe/app/src/test/java/dev/shorthouse/remindme/viewmodel/ListContainerViewModelTest.kt
+++ RemindMe/app/src/test/java/dev/shorthouse/remindme/viewmodel/ListContainerViewModelTest.kt
@@ -1,6 +1,8 @@
 package dev.shorthouse.remindme.viewmodel
 
 import com.google.common.truth.Truth.assertThat
+import dev.shorthouse.remindme.R
+import dev.shorthouse.remindme.utilities.ReminderSort
 import org.junit.Test
 
 class ListContainerViewModelTest {
@@ -8,11 +10,62 @@ class ListContainerViewModelTest {
     private val viewModel = ListContainerViewModel()
 
     @Test
-    fun `menu item to sort map, is correctly mapped`() {
-        assertThat(viewModel.sortToMenuItemMap.keys.toString())
-            .isEqualTo(viewModel.menuItemToSortMap.values.toString())
+    fun `Reminder sort to item map, has expected map values`() {
+        val expectedMap = mapOf(
+            ReminderSort.EARLIEST_DATE_FIRST to R.id.drawer_sort_earliest_date_first,
+            ReminderSort.LATEST_DATE_FIRST to R.id.drawer_sort_latest_date_first
+        )
 
-        assertThat(viewModel.sortToMenuItemMap.values.toString())
-            .isEqualTo(viewModel.menuItemToSortMap.keys.toString())
+        assertThat(viewModel.sortToMenuItemMap).isEqualTo(expectedMap)
+    }
+
+    @Test
+    fun `Reminder menu item to sort map, has expected values`() {
+        val expectedMap = mapOf(
+            R.id.drawer_sort_earliest_date_first to ReminderSort.EARLIEST_DATE_FIRST,
+            R.id.drawer_sort_latest_date_first to ReminderSort.LATEST_DATE_FIRST
+        )
+
+        assertThat(viewModel.menuItemToSortMap).isEqualTo(expectedMap)
+    }
+
+    @Test
+    fun `Is item changed with equal items, returns false`() {
+        val itemOneId = 1
+        val itemTwoId = 1
+
+        val isItemChanged = viewModel.isItemChanged(itemOneId, itemTwoId)
+
+        assertThat(isItemChanged).isFalse()
+    }
+
+    @Test
+    fun `Is item changed with different items, returns true`() {
+        val itemOneId = 1
+        val itemTwoId = 2
+
+        val isItemChanged = viewModel.isItemChanged(itemOneId, itemTwoId)
+
+        assertThat(isItemChanged).isTrue()
+    }
+
+    @Test
+    fun `Get toolbar title for active reminders, returns expected title`() {
+        val expectedToolbarTitle = R.string.active_reminders
+        viewModel.bottomSheetListSelection = R.id.drawer_active_list
+
+        val toolbarTitle = viewModel.getToolbarTitle()
+
+        assertThat(toolbarTitle).isEqualTo(expectedToolbarTitle)
+    }
+
+    @Test
+    fun `Get toolbar title for all reminders, returns expected title`() {
+        val expectedToolbarTitle = R.string.all_reminders
+        viewModel.bottomSheetListSelection = R.id.drawer_all_list
+
+        val toolbarTitle = viewModel.getToolbarTitle()
+
+        assertThat(toolbarTitle).isEqualTo(expectedToolbarTitle)
     }
 }
