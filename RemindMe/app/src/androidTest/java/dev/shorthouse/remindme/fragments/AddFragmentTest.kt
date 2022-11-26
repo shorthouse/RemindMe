7c0d0b785de85b32599bee0de57ddae9bc308605
Harry S <shorthouse20@gmail.com>
2022-11-26 12:50:59 +0000

    Update tests for radio buttons

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/fragments/AddFragmentTest.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/fragments/AddFragmentTest.kt
@@ -4,21 +4,18 @@ import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.action.ViewActions.swipeUp
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isFocused
-import androidx.test.espresso.matcher.ViewMatchers.withHint
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.launchFragmentInHiltContainer
 import dev.shorthouse.remindme.util.setTextInTextView
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Matchers.allOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -77,41 +74,57 @@ class AddFragmentTest {
     }
 
     @Test
-    fun when_enable_repeat_reminder_toggle_should_display_repeat_dropdown() {
+    fun when_enable_repeat_reminder_toggle_should_display_repeat_selector() {
         onView(withId(R.id.repeat_switch)).perform(click())
         onView(withId(R.id.repeats_every_header)).check(matches(withText("Repeats every")))
         onView(withId(R.id.repeat_value_label)).check(matches(isDisplayed()))
         onView(withId(R.id.repeat_value_input)).check(matches(withText("1")))
-        onView(withId(R.id.repeat_unit_label)).check(matches(isDisplayed()))
-        onView(withId(R.id.repeat_unit_input)).check(matches(withText("day")))
+        onView(withId(R.id.repeat_unit_radio_group)).check(matches(isDisplayed()))
+        onView(withId(R.id.repeat_unit_days)).check(matches(isDisplayed()))
+        onView(withId(R.id.repeat_unit_weeks)).check(matches(isDisplayed()))
+        onView(withId(R.id.repeat_unit_days)).check(matches(isChecked()))
+        onView(withId(R.id.repeat_unit_weeks)).check(matches(isNotChecked()))
+        onView(allOf(withText("Day"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
+        onView(allOf(withText("Week"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
     }
 
     @Test
     fun when_repeat_value_changed_to_singular_should_display_singular_repeat_unit() {
         onView(withId(R.id.repeat_switch)).perform(click())
         onView(withId(R.id.repeat_value_input)).perform(replaceText("1"))
-        onView(withId(R.id.repeat_unit_input)).check(matches(withText("day")))
+        onView(allOf(withText("Day"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
+        onView(allOf(withText("Week"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
     }
 
     @Test
     fun when_repeat_value_changed_to_plural_should_display_plural_repeat_unit() {
         onView(withId(R.id.repeat_switch)).perform(click())
         onView(withId(R.id.repeat_value_input)).perform(replaceText("2"))
-        onView(withId(R.id.repeat_unit_input)).check(matches(withText("days")))
+        onView(allOf(withText("Days"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
+        onView(allOf(withText("Weeks"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
     }
 
     @Test
     fun when_repeat_value_changed_to_empty_should_display_singular_repeat_unit() {
         onView(withId(R.id.repeat_switch)).perform(click())
         onView(withId(R.id.repeat_value_input)).perform(replaceText(""))
-        onView(withId(R.id.repeat_unit_input)).check(matches(withText("days")))
+        onView(allOf(withText("Days"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
+        onView(allOf(withText("Weeks"), withParent(withId(R.id.repeat_unit_radio_group))))
+            .check(matches(isDisplayed()))
     }
 
     @Test
     fun when_large_notes_input_should_expand_to_scrollable_view() {
         onView(withId(R.id.notes_input)).perform(setTextInTextView("m".repeat(1000)))
         Espresso.closeSoftKeyboard()
-        onView(withId(R.id.add_edit_scroll_view)).perform(swipeUp())
-        onView(withId(R.id.repeat_header)).check(matches(isDisplayed()))
+        onView(withId(R.id.repeat_header)).perform(scrollTo())
+        onView(withId(R.id.repeat_header)).check(matches(isCompletelyDisplayed()))
     }
 }
