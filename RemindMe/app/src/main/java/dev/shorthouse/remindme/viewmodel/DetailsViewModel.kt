9ce02d4fa89f78c88a77a86abea80814a55a3b10
Harry S <shorthouse20@gmail.com>
2022-11-30 21:33:51 +0000

    Add display reminder logic

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/DetailsViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/DetailsViewModel.kt
@@ -4,15 +4,13 @@ import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.data.ReminderRepository
-import dev.shorthouse.remindme.model.RepeatInterval
 import dev.shorthouse.remindme.di.IoDispatcher
+import dev.shorthouse.remindme.model.DisplayReminder
 import dev.shorthouse.remindme.model.Reminder
-import dev.shorthouse.remindme.utilities.DATE_INPUT_PATTERN
+import dev.shorthouse.remindme.model.RepeatInterval
 import dev.shorthouse.remindme.utilities.NotificationScheduler
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 
@@ -26,6 +24,10 @@ class DetailsViewModel @Inject constructor(
     private val reminderId = state.get<Long>("id") ?: 1L
 
     val reminder = repository.getReminder(reminderId).asLiveData()
+
+    val displayReminder: LiveData<DisplayReminder> = Transformations.map(reminder) { reminder ->
+        reminder.createDisplayReminder()
+    }
 
     fun getReminder(id: Long): LiveData<Reminder> {
         return repository.getReminder(id).asLiveData()
@@ -55,18 +57,13 @@ class DetailsViewModel @Inject constructor(
         }
     }
 
-    fun getRepeatIntervalStringId(repeatInterval: RepeatInterval): Int {
+    fun getRepeatIntervalStringId(repeatInterval: RepeatInterval?): Int? {
+        if (repeatInterval == null) return null
+
         return when (repeatInterval.timeUnit) {
             ChronoUnit.DAYS -> R.plurals.interval_days
             else -> R.plurals.interval_weeks
         }
-    }
-
-    fun getFormattedDate(zonedDateTime: ZonedDateTime): String {
-        return zonedDateTime
-            .toLocalDate()
-            .format(DateTimeFormatter.ofPattern(DATE_INPUT_PATTERN))
-            .toString()
     }
 
     private fun cancelReminderNotification() {
