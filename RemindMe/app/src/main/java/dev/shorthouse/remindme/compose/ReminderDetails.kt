12670ff21d6dd6bf81b4de77851d44ac66b3f14e
Harry S <shorthouse20@gmail.com>
2022-12-23 16:59:12 +0000

    Refactor to use shared iconandtext composable

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/compose/ReminderDetails.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/compose/ReminderDetails.kt
@@ -1,26 +1,24 @@
 package dev.shorthouse.remindme.compose
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.*
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.android.material.composethemeadapter.MdcTheme
 import dev.shorthouse.remindme.R
-import dev.shorthouse.remindme.model.DisplayReminder
-import dev.shorthouse.remindme.model.DisplayRepeatInterval
+import dev.shorthouse.remindme.compose.state.ReminderState
 import dev.shorthouse.remindme.viewmodel.DetailsViewModel
 
 @Composable
@@ -39,16 +37,22 @@ fun ReminderDetailsScreen(
         onNavigateUp()
     }
 
-    val reminder by detailsViewModel.displayReminder.observeAsState()
+    val reminder by detailsViewModel.reminder.observeAsState()
 
     reminder?.let {
-        ReminderDetailsScreenContent(it, onNavigateUp, onEdit, onDelete, onComplete)
+        ReminderDetailsScaffold(
+            reminderState = ReminderState(it),
+            onNavigateUp = onNavigateUp,
+            onEdit = onEdit,
+            onDelete = onDelete,
+            onComplete = onComplete
+        )
     }
 }
 
 @Composable
-fun ReminderDetailsScreenContent(
-    reminder: DisplayReminder,
+fun ReminderDetailsScaffold(
+    reminderState: ReminderState,
     onNavigateUp: () -> Unit,
     onEdit: () -> Unit,
     onDelete: () -> Unit,
@@ -64,7 +68,10 @@ fun ReminderDetailsScreenContent(
             )
         },
         content = { innerPadding ->
-            ReminderDetailsContent(reminder, innerPadding)
+            ReminderDetailsContent(
+                innerPadding = innerPadding,
+                reminderState = reminderState
+            )
         }
     )
 }
@@ -82,8 +89,8 @@ fun ReminderDetailsTopBar(
 
     if (isDeleteDialogShown) {
         DetailsAlertDialog(
-            title = "Delete this reminder?",
-            confirmText = "Delete",
+            title = stringResource(R.string.alert_dialog_title_delete),
+            confirmText = stringResource(R.string.alert_dialog_confirm_delete),
             onConfirm = onDelete,
             onDismiss = { isDeleteDialogShown = false }
         )
@@ -91,8 +98,8 @@ fun ReminderDetailsTopBar(
 
     if (isCompleteDialogShown) {
         DetailsAlertDialog(
-            title = "Complete this reminder?",
-            confirmText = "Complete",
+            title = stringResource(R.string.alert_dialog_title_complete),
+            confirmText = stringResource(R.string.alert_dialog_confirm_complete),
             onConfirm = onComplete,
             onDismiss = { isDeleteDialogShown = false }
         )
@@ -153,96 +160,67 @@ fun ReminderDetailsTopBar(
     )
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun ReminderDetailsContent(displayReminder: DisplayReminder, innerPadding: PaddingValues) {
+fun ReminderDetailsContent(
+    innerPadding: PaddingValues,
+    reminderState: ReminderState
+) {
     Column(
-        Modifier
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.margin_large)),
+        modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
             .padding(
                 start = dimensionResource(R.dimen.margin_normal),
+                top = innerPadding.calculateTopPadding() + dimensionResource(R.dimen.margin_large),
                 end = dimensionResource(R.dimen.margin_normal),
-                top = innerPadding.calculateTopPadding(),
+                bottom = dimensionResource(R.dimen.margin_large)
             )
     ) {
-        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.margin_normal)))
-
-        ReminderName(displayReminder.name)
-
-        ReminderDetailRow(
-            R.drawable.ic_calendar,
-            R.string.cd_icon_calendar,
-            displayReminder.startDate
+        ReminderName(
+            name = reminderState.name,
         )
 
-        ReminderDetailRow(
-            R.drawable.ic_clock,
-            R.string.cd_icon_clock,
-            displayReminder.startTime
-        )
-
-        if (displayReminder.repeatInterval != null) {
-            ReminderDetailRow(
-                R.drawable.ic_repeat,
-                R.string.cd_icon_repeat,
-                pluralStringResource(
-                    displayReminder.repeatInterval.pluralId,
-                    displayReminder.repeatInterval.pluralCount,
-                    displayReminder.repeatInterval.pluralCount
-                ),
-            )
+        val detailIcons = buildList {
+            add(painterResource(R.drawable.ic_calendar))
+            add(painterResource(R.drawable.ic_clock))
+            if (reminderState.isRepeatReminder) add(painterResource(R.drawable.ic_repeat))
+            if (reminderState.isNotificationSent) add(painterResource(R.drawable.ic_notification_outline))
+            reminderState.notes?.let { add(painterResource(R.drawable.ic_notes)) }
         }
 
-        if (displayReminder.isNotificationSent) {
-            ReminderDetailRow(
-                R.drawable.ic_notification_outline,
-                R.string.cd_icon_notification,
-                stringResource(R.string.notifications_on),
+        val detailTexts = buildList {
+            add(reminderState.date)
+            add(reminderState.time)
+            if (reminderState.isRepeatReminder) add(
+                stringResource(
+                    R.string.reminder_details_repeat_interval,
+                    reminderState.repeatAmount,
+                    reminderState.repeatUnit
+                )
             )
+            if (reminderState.isNotificationSent) add(stringResource(R.string.notifications_on))
+            reminderState.notes?.let { add(it) }
         }
 
-        if (displayReminder.notes != null) {
-            ReminderDetailRow(
-                R.drawable.ic_notes,
-                R.string.cd_icon_notes,
-                displayReminder.notes
+        repeat(detailIcons.size) { i ->
+            TextWithLeftIcon(
+                icon = detailIcons[i],
+                text = detailTexts[i],
             )
         }
-
-        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.margin_small)))
     }
 }
 
 @Composable
-fun ReminderName(name: String) {
+fun ReminderName(
+    name: String,
+) {
     Text(
         text = name,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
     )
-}
-
-@Composable
-fun ReminderDetailRow(
-    @DrawableRes iconId: Int,
-    @StringRes iconContentDescriptionId: Int,
-    detailText: String,
-) {
-    Row(Modifier.padding(top = dimensionResource(R.dimen.margin_large))) {
-        Image(
-            painter = painterResource(iconId),
-            contentDescription = stringResource(iconContentDescriptionId),
-        )
-
-        Text(
-            text = detailText,
-            fontSize = 18.sp,
-            color = colorResource(R.color.on_primary),
-            modifier = Modifier
-                .padding(start = dimensionResource(R.dimen.margin_normal))
-        )
-    }
 }
 
 @Composable
@@ -274,18 +252,23 @@ private fun DetailsAlertDialog(
 @Composable
 private fun ReminderDetailsScreenPreview() {
     MdcTheme {
-        val reminder = DisplayReminder(
-            id = 1,
-            name = "Yoga tonight",
-            startDate = "Wed, 22 Mar 2000",
-            startTime = "14:30",
-            isNotificationSent = true,
-            repeatInterval = DisplayRepeatInterval(R.plurals.interval_weeks, 2),
-            notes = "Don't forget to warm up!",
-        )
+        val reminderState by remember {
+            mutableStateOf(
+                ReminderState(
+                    name = "Yoga tonight",
+                    date = "Wed, 22 Mar 2000",
+                    time = "14:30",
+                    isNotificationSent = true,
+                    isRepeatReminder = true,
+                    repeatAmount = "2",
+                    repeatUnit = "Weeks",
+                    notes = "Don't forget to warm up!"
+                )
+            )
+        }
 
-        ReminderDetailsScreenContent(
-            reminder = reminder,
+        ReminderDetailsScaffold(
+            reminderState = reminderState,
             onNavigateUp = {},
             onEdit = {},
             onDelete = {},
@@ -299,8 +282,8 @@ private fun ReminderDetailsScreenPreview() {
 private fun DetailsAlertDialogPreview() {
     MdcTheme {
         DetailsAlertDialog(
-            title = "Delete this reminder?",
-            confirmText = "Delete",
+            title = stringResource(R.string.alert_dialog_title_delete),
+            confirmText = stringResource(R.string.alert_dialog_confirm_delete),
             onConfirm = {},
             onDismiss = {}
         )
