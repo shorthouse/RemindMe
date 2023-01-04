dae873a648dd373b32a31797db0fe17e8b71b7be
Harry S <shorthouse20@gmail.com>
2023-01-04 17:35:50 +0000

    Add get error message function

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/AddViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/AddViewModel.kt
@@ -3,10 +3,12 @@ package dev.shorthouse.remindme.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.data.ReminderRepository
 import dev.shorthouse.remindme.di.IoDispatcher
 import dev.shorthouse.remindme.model.Reminder
 import dev.shorthouse.remindme.utilities.NotificationScheduler
+import dev.shorthouse.remindme.utilities.UiText
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import java.time.ZonedDateTime
@@ -33,5 +35,12 @@ class AddViewModel @Inject constructor(
     fun isReminderValid(reminder: Reminder): Boolean {
         return reminder.name.isNotBlank() &&
                 reminder.startDateTime.isAfter(ZonedDateTime.now())
+    }
+
+    fun getErrorMessage(reminder: Reminder): UiText.StringResource {
+        return when {
+            reminder.name.isBlank() -> UiText.StringResource(R.string.error_name_empty)
+            else -> UiText.StringResource(R.string.error_time_past)
+        }
     }
 }
