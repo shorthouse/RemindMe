8d03f766ff7cc0b8bc215a7b603b372d3a0cec0d
Harry S <shorthouse20@gmail.com>
2023-01-07 14:06:03 +0000

    Add required @HiltViewModel tag

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/EditViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/EditViewModel.kt
@@ -3,9 +3,11 @@ package dev.shorthouse.remindme.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shorthouse.remindme.data.ReminderRepository
 import javax.inject.Inject
 
+@HiltViewModel
 class EditViewModel @Inject constructor(
     repository: ReminderRepository,
     state: SavedStateHandle
