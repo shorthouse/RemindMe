9b07c770fd94fcffd55730f5860f91b96ad73239
Harry S <shorthouse20@gmail.com>
2023-01-07 13:23:35 +0000

    Streamline to just host edit reminder

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/EditViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/EditViewModel.kt
@@ -3,32 +3,13 @@ package dev.shorthouse.remindme.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import dev.shorthouse.remindme.data.ReminderRepository
-import dev.shorthouse.remindme.di.IoDispatcher
-import dev.shorthouse.remindme.model.Reminder
-import dev.shorthouse.remindme.utilities.NotificationScheduler
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class EditViewModel @Inject constructor(
-    private val repository: ReminderRepository,
-    private val notificationScheduler: NotificationScheduler,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    repository: ReminderRepository,
     state: SavedStateHandle
 ) : ViewModel() {
     private val reminderId = state.get<Long>("id") ?: 1L
     val reminder = repository.getReminder(reminderId).asLiveData()
-
-    fun editReminder(reminder: Reminder) {
-        viewModelScope.launch(ioDispatcher) {
-            repository.updateReminder(reminder)
-            notificationScheduler.cancelExistingReminderNotification(reminder)
-
-            if (reminder.isNotificationSent) {
-                notificationScheduler.scheduleReminderNotification(reminder)
-            }
-        }
-    }
 }
