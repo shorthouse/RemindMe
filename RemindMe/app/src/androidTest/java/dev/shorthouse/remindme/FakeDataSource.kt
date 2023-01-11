c14b4f5b88a34bd64fc00ba16c4d8af5e9f2aa35
Harry S <shorthouse20@gmail.com>
2023-01-11 20:03:49 +0000

    Rename to getAllReminders()

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/FakeDataSource.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/FakeDataSource.kt
@@ -15,7 +15,7 @@ class FakeDataSource(private var reminders: MutableList<Reminder> = mutableListO
         return flowOf(reminders.first { it.id == id })
     }
 
-    override fun getActiveNotCompletedReminders(nowDateTime: ZonedDateTime): Flow<List<Reminder>> {
+    override fun getActiveReminders(nowDateTime: ZonedDateTime): Flow<List<Reminder>> {
         return flowOf(
             reminders.filter {
                 it.startDateTime.isBefore(nowDateTime) || it.startDateTime.isEqual(nowDateTime)
@@ -23,7 +23,7 @@ class FakeDataSource(private var reminders: MutableList<Reminder> = mutableListO
         )
     }
 
-    override fun getNotCompletedReminders(): Flow<List<Reminder>> {
+    override fun getAllReminders(): Flow<List<Reminder>> {
         return flowOf(reminders.filter { !it.isComplete })
     }
 
