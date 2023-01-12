38312c47ab37867858ec9a8ecd600d6533dbb66d
Harry S <shorthouse20@gmail.com>
2023-01-12 21:33:24 +0000

    Remove now unused binding adapters

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/BindingAdapters.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/BindingAdapters.kt
@@ -9,16 +9,6 @@ fun showIfRepeatReminder(view: View, repeatInterval: RepeatInterval?) {
     view.visibility = if (repeatInterval != null) View.VISIBLE else View.GONE
 }
 
-@BindingAdapter("showIfNotesExist")
-fun showIfNotesExist(view: View, notes: String?) {
-    view.visibility = if (notes != null) View.VISIBLE else View.GONE
-}
-
-@BindingAdapter("showIfRepeatChecked")
-fun showIfRepeatChecked(view: View, isRepeatChecked: Boolean) {
-    view.visibility = if (isRepeatChecked) View.VISIBLE else View.GONE
-}
-
 @BindingAdapter("showIfNotificationEnabled")
 fun showIfNotificationEnabled(view: View, isNotificationEnabled: Boolean) {
     view.visibility = if (isNotificationEnabled) View.VISIBLE else View.GONE
