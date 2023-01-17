6d7202fed190fa2e19ca7b98bbf3c6eda302d021
Harry S <shorthouse20@gmail.com>
2023-01-17 21:14:57 +0000

    Refactor to use compose navigation

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/EditViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/EditViewModel.kt
@@ -1,17 +1,18 @@
 package dev.shorthouse.remindme.viewmodel
 
-import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shorthouse.remindme.data.ReminderRepository
+import dev.shorthouse.remindme.model.Reminder
 import javax.inject.Inject
 
 @HiltViewModel
 class EditViewModel @Inject constructor(
-    repository: ReminderRepository,
-    state: SavedStateHandle
+    private val repository: ReminderRepository
 ) : ViewModel() {
-    private val reminderId = state.get<Long>("id") ?: 1L
-    val reminder = repository.getReminder(reminderId).asLiveData()
+    fun getReminder(reminderId: Long): LiveData<Reminder> {
+        return repository.getReminder(reminderId).asLiveData()
+    }
 }
