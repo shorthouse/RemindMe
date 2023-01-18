4e54ec8fa3e77c8e98b6e8aadf45a15555613c54
Harry S <shorthouse20@gmail.com>
2023-01-18 18:53:43 +0000

    Comment out disused tests

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/fragments/ActiveListFragmentTest.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/fragments/ActiveListFragmentTest.kt
@@ -1,69 +1,41 @@
 package dev.shorthouse.remindme.fragments
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isClickable
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
-import dagger.hilt.components.SingletonComponent
-import dev.shorthouse.remindme.FakeDataSource
-import dev.shorthouse.remindme.R
-import dev.shorthouse.remindme.data.ReminderDataSource
-import dev.shorthouse.remindme.model.RepeatInterval
-import dev.shorthouse.remindme.di.DataSourceModule
-import dev.shorthouse.remindme.launchFragmentInHiltContainer
-import dev.shorthouse.remindme.util.TestUtil
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.hamcrest.CoreMatchers.allOf
-import org.junit.Rule
-import org.junit.Test
-import java.time.ZonedDateTime
-import java.time.temporal.ChronoUnit
-import javax.inject.Singleton
-
-@ExperimentalCoroutinesApi
-@HiltAndroidTest
-@UninstallModules(DataSourceModule::class)
-class ActiveListFragmentTest {
-
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
-
-    @Module
-    @InstallIn(SingletonComponent::class)
-    class TestModule {
-        @Singleton
-        @Provides
-        fun provideReminderDataSource(): ReminderDataSource {
-            val reminders = mutableListOf(
-                TestUtil.createReminder(
-                    name = "Test Active Reminder",
-                    startDateTime = ZonedDateTime.parse("2000-01-01T14:02:00Z"),
-                    isNotificationSent = true,
-                    repeatInterval = RepeatInterval(1, ChronoUnit.DAYS)
-                )
-            )
-
-            return FakeDataSource(reminders)
-        }
-    }
-
-    @Test
-    fun when_active_reminder_exists_should_display_correctly() {
-        launchFragmentInHiltContainer<ListActiveFragment>()
-
-        onView(withId(R.id.reminder_name)).check(matches(withText("Test Active Reminder")))
-        onView(withId(R.id.reminder_date)).check(matches(withText("01 Jan 2000")))
-        onView(withId(R.id.reminder_time)).check(matches(withText("14:02")))
-        onView(withId(R.id.done_checkbox)).check(matches(allOf(isClickable(), isDisplayed())))
-        onView(withId(R.id.notification_icon)).check(matches(isDisplayed()))
-        onView(withId(R.id.repeat_icon)).check(matches(isDisplayed()))
-    }
-}
+//@ExperimentalCoroutinesApi
+//@HiltAndroidTest
+//@UninstallModules(DataSourceModule::class)
+//class ActiveListFragmentTest {
+//
+//    @get:Rule
+//    var hiltRule = HiltAndroidRule(this)
+//
+//    @Module
+//    @InstallIn(SingletonComponent::class)
+//    class TestModule {
+//        @Singleton
+//        @Provides
+//        fun provideReminderDataSource(): ReminderDataSource {
+//            val reminders = mutableListOf(
+//                TestUtil.createReminder(
+//                    name = "Test Active Reminder",
+//                    startDateTime = ZonedDateTime.parse("2000-01-01T14:02:00Z"),
+//                    isNotificationSent = true,
+//                    repeatInterval = RepeatInterval(1, ChronoUnit.DAYS)
+//                )
+//            )
+//
+//            return FakeDataSource(reminders)
+//        }
+//    }
+//
+//    @Test
+//    fun when_active_reminder_exists_should_display_correctly() {
+//        launchFragmentInHiltContainer<ListActiveFragment>()
+//
+//        onView(withId(R.id.reminder_name)).check(matches(withText("Test Active Reminder")))
+//        onView(withId(R.id.reminder_date)).check(matches(withText("01 Jan 2000")))
+//        onView(withId(R.id.reminder_time)).check(matches(withText("14:02")))
+//        onView(withId(R.id.done_checkbox)).check(matches(allOf(isClickable(), isDisplayed())))
+//        onView(withId(R.id.notification_icon)).check(matches(isDisplayed()))
+//        onView(withId(R.id.repeat_icon)).check(matches(isDisplayed()))
+//    }
+//}
--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/fragments/AllListFragmentTest.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/fragments/AllListFragmentTest.kt
@@ -1,61 +1,61 @@
 package dev.shorthouse.remindme.fragments
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
-import dagger.hilt.components.SingletonComponent
-import dev.shorthouse.remindme.FakeDataSource
-import dev.shorthouse.remindme.R
-import dev.shorthouse.remindme.data.ReminderDataSource
-import dev.shorthouse.remindme.di.DataSourceModule
-import dev.shorthouse.remindme.launchFragmentInHiltContainer
-import dev.shorthouse.remindme.util.TestUtil
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Rule
-import org.junit.Test
-import java.time.ZonedDateTime
-import javax.inject.Singleton
-
-@ExperimentalCoroutinesApi
-@HiltAndroidTest
-@UninstallModules(DataSourceModule::class)
-class AllListFragmentTest {
-
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
-
-    @Module
-    @InstallIn(SingletonComponent::class)
-    class TestModule {
-        @Singleton
-        @Provides
-        fun provideReminderDataSource(): ReminderDataSource {
-            val reminders = mutableListOf(
-                TestUtil.createReminder(
-                    name = "Test Reminder",
-                    startDateTime = ZonedDateTime.parse("3000-01-01T09:07:00Z")
-                )
-            )
-
-            return FakeDataSource(reminders)
-        }
-    }
-
-    @Test
-    fun when_all_reminder_exists_should_display_correctly() {
-        launchFragmentInHiltContainer<ListAllFragment>(
-            themeResId = R.style.Theme_RemindMe
-        )
-
-        onView(withId(R.id.reminder_name)).check(matches(withText("Test Reminder")))
-        onView(withId(R.id.reminder_date)).check(matches(withText("01 Jan 3000")))
-        onView(withId(R.id.reminder_time)).check(matches(withText("09:07")))
-    }
-}
+//import androidx.test.espresso.Espresso.onView
+//import androidx.test.espresso.assertion.ViewAssertions.matches
+//import androidx.test.espresso.matcher.ViewMatchers.withId
+//import androidx.test.espresso.matcher.ViewMatchers.withText
+//import dagger.Module
+//import dagger.Provides
+//import dagger.hilt.InstallIn
+//import dagger.hilt.android.testing.HiltAndroidRule
+//import dagger.hilt.android.testing.HiltAndroidTest
+//import dagger.hilt.android.testing.UninstallModules
+//import dagger.hilt.components.SingletonComponent
+//import dev.shorthouse.remindme.FakeDataSource
+//import dev.shorthouse.remindme.R
+//import dev.shorthouse.remindme.data.ReminderDataSource
+//import dev.shorthouse.remindme.di.DataSourceModule
+//import dev.shorthouse.remindme.launchFragmentInHiltContainer
+//import dev.shorthouse.remindme.util.TestUtil
+//import kotlinx.coroutines.ExperimentalCoroutinesApi
+//import org.junit.Rule
+//import org.junit.Test
+//import java.time.ZonedDateTime
+//import javax.inject.Singleton
+//
+//@ExperimentalCoroutinesApi
+//@HiltAndroidTest
+//@UninstallModules(DataSourceModule::class)
+//class AllListFragmentTest {
+//
+//    @get:Rule
+//    var hiltRule = HiltAndroidRule(this)
+//
+//    @Module
+//    @InstallIn(SingletonComponent::class)
+//    class TestModule {
+//        @Singleton
+//        @Provides
+//        fun provideReminderDataSource(): ReminderDataSource {
+//            val reminders = mutableListOf(
+//                TestUtil.createReminder(
+//                    name = "Test Reminder",
+//                    startDateTime = ZonedDateTime.parse("3000-01-01T09:07:00Z")
+//                )
+//            )
+//
+//            return FakeDataSource(reminders)
+//        }
+//    }
+//
+//    @Test
+//    fun when_all_reminder_exists_should_display_correctly() {
+//        launchFragmentInHiltContainer<ListAllFragment>(
+//            themeResId = R.style.Theme_RemindMe
+//        )
+//
+//        onView(withId(R.id.reminder_name)).check(matches(withText("Test Reminder")))
+//        onView(withId(R.id.reminder_date)).check(matches(withText("01 Jan 3000")))
+//        onView(withId(R.id.reminder_time)).check(matches(withText("09:07")))
+//    }
+//}
