cdc96640d7e7de1f5d9761a2a9116e2f7938d51d
Harry S <shorthouse20@gmail.com>
2023-01-24 19:30:51 +0000

    Remove unused resources

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/utilities/Constants.kt
@@ -1,8 +1,3 @@
 package dev.shorthouse.remindme.utilities
 
 const val DAYS_IN_WEEK = 7
-
-const val DATE_PATTERN = "dd MMM yyyy"
-
-const val SCRIM_START_ALPHA = 0.6f
-const val SCRIM_LERP_MAX = 255f
