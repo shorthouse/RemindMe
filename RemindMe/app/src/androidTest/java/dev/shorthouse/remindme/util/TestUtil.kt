06483704cb0e6ffa3893b9042db8916044dd4528
Harry S <shorthouse20@gmail.com>
2023-01-27 21:33:37 +0000

    Refactor TestUtil to use companion object

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/util/TestUtil.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/util/TestUtil.kt
@@ -1,27 +1,143 @@
 package dev.shorthouse.remindme.util
 
-import dev.shorthouse.remindme.model.RepeatInterval
+import dev.shorthouse.remindme.compose.preview.PreviewData
+import dev.shorthouse.remindme.compose.state.ReminderState
 import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.model.RepeatInterval
+import java.time.LocalTime
 import java.time.ZonedDateTime
 
-object TestUtil {
+class TestUtil {
+    companion object {
+        val reminderAddState = ReminderState(
+            id = 1,
+            name = "",
+            date = "Sat, 01 Jan 2000",
+            time = LocalTime.of(14, 30),
+            isNotificationSent = false,
+            isRepeatReminder = false,
+            repeatAmount = "1",
+            repeatUnit = "Day",
+            notes = "",
+            isCompleted = false
+        )
+
+        val reminderEditState = ReminderState(
+            id = 1,
+            name = "",
+            date = "Sat, 01 Jan 2000",
+            time = LocalTime.of(14, 30),
+            isNotificationSent = false,
+            isRepeatReminder = false,
+            repeatAmount = "1",
+            repeatUnit = "Day",
+            notes = "",
+            isCompleted = false
+        )
+
+        val reminderListStates = listOf(
+            PreviewData.reminderState,
+            ReminderState(
+                id = 2,
+                name = "Feed the fish",
+                date = "Fri, 27 Jun 2017",
+                time = LocalTime.of(18, 15),
+                isNotificationSent = true,
+                isRepeatReminder = false,
+                repeatAmount = "",
+                repeatUnit = "",
+                notes = null,
+                isCompleted = false
+            ),
+            ReminderState(
+                id = 3,
+                name = "Go for a run",
+                date = "Wed, 07 Jan 2022",
+                time = LocalTime.of(7, 15),
+                isNotificationSent = false,
+                isRepeatReminder = false,
+                repeatAmount = "",
+                repeatUnit = "",
+                notes = "The cardio will be worth it!",
+                isCompleted = false
+            )
+        )
+        val reminderStateList = listOf(
+            PreviewData.reminderState,
+            ReminderState(
+                id = 2,
+                name = "Feed the fish",
+                date = "Fri, 27 Jun 2017",
+                time = LocalTime.of(18, 15),
+                isNotificationSent = true,
+                isRepeatReminder = false,
+                repeatAmount = "",
+                repeatUnit = "",
+                notes = null,
+                isCompleted = false
+            ),
+            ReminderState(
+                id = 3,
+                name = "Go for a run",
+                date = "Wed, 07 Jan 2022",
+                time = LocalTime.of(7, 15),
+                isNotificationSent = false,
+                isRepeatReminder = false,
+                repeatAmount = "",
+                repeatUnit = "",
+                notes = "The cardio will be worth it!",
+                isCompleted = false
+            )
+        )
+    }
+
     fun createReminder(
         id: Long = 1L,
         name: String = "test reminder",
         startDateTime: ZonedDateTime = ZonedDateTime.now(),
+        isNotificationSent: Boolean = false,
         repeatInterval: RepeatInterval? = null,
         notes: String? = null,
-        isArchived: Boolean = false,
-        isNotificationSent: Boolean = false
+        isCompleted: Boolean = false
     ): Reminder {
         return Reminder(
             id = id,
             name = name,
             startDateTime = startDateTime,
+            isNotificationSent = isNotificationSent,
             repeatInterval = repeatInterval,
             notes = notes,
-            isCompleted = isArchived,
-            isNotificationSent = isNotificationSent
+            isCompleted = isCompleted
+        )
+    }
+
+    fun createReminderAddState(): ReminderState {
+        return ReminderState(
+            id = 1,
+            name = "",
+            date = "Sat, 01 Jan 2000",
+            time = LocalTime.of(14, 30),
+            isNotificationSent = false,
+            isRepeatReminder = false,
+            repeatAmount = "1",
+            repeatUnit = "Day",
+            notes = "",
+            isCompleted = false
+        )
+    }
+
+    fun createReminderEditState(): ReminderState {
+        return ReminderState(
+            id = 1,
+            name = "Reminder name",
+            date = "Sun, 02 Jan 2000",
+            time = LocalTime.of(18, 30),
+            isNotificationSent = true,
+            isRepeatReminder = true,
+            repeatAmount = "2",
+            repeatUnit = "Weeks",
+            notes = "Reminder notes",
+            isCompleted = false
         )
     }
 }
