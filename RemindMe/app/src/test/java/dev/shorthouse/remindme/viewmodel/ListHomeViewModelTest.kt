1c7cf4823b9237f329e432a2267e4ae6de50e61c
Harry S <shorthouse20@gmail.com>
2023-01-28 18:15:50 +0000

    Add list home view model test

---
+++ RemindMe/app/src/test/java/dev/shorthouse/remindme/viewmodel/ListHomeViewModelTest.kt
@@ -0,0 +1,69 @@
+package dev.shorthouse.remindme.viewmodel
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import dev.shorthouse.remindme.utilities.enums.ReminderList
+import dev.shorthouse.remindme.utilities.enums.ReminderSortOrder
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ListHomeViewModelTest {
+    private lateinit var listHomeViewModel: ListHomeViewModel
+
+    @Before
+    fun setup() {
+        listHomeViewModel = ListHomeViewModel()
+    }
+
+    @Test
+    fun `Set sort index to 0, sort order is set to earliest date first`() {
+        listHomeViewModel.selectedSortIndex = 0
+        val expectedSortOrder = ReminderSortOrder.EARLIEST_DATE_FIRST
+
+        val sortOrder = listHomeViewModel.selectedReminderSortOrder
+
+        assertThat(sortOrder).isEqualTo(expectedSortOrder)
+    }
+
+    @Test
+    fun `Set sort index to 1, sort order is set to latest date first`() {
+        listHomeViewModel.selectedSortIndex = 1
+        val expectedSortOrder = ReminderSortOrder.LATEST_DATE_FIRST
+
+        val sortOrder = listHomeViewModel.selectedReminderSortOrder
+
+        assertThat(sortOrder).isEqualTo(expectedSortOrder)
+    }
+
+    @Test
+    fun `Set navigate index to 0, list becomes overdue reminders`() {
+        listHomeViewModel.selectedNavigateIndex = 0
+        val expectedReminderList = ReminderList.OVERDUE
+
+        val reminderList = listHomeViewModel.selectedReminderList
+
+        assertThat(reminderList).isEqualTo(expectedReminderList)
+    }
+
+    @Test
+    fun `Set navigate index to 1, list becomes scheduled reminders`() {
+        listHomeViewModel.selectedNavigateIndex = 1
+        val expectedReminderList = ReminderList.SCHEDULED
+
+        val reminderList = listHomeViewModel.selectedReminderList
+
+        assertThat(reminderList).isEqualTo(expectedReminderList)
+    }
+
+    @Test
+    fun `Set navigate index to 2, list becomes completed reminders`() {
+        listHomeViewModel.selectedNavigateIndex = 2
+        val expectedReminderList = ReminderList.COMPLETED
+
+        val reminderList = listHomeViewModel.selectedReminderList
+
+        assertThat(reminderList).isEqualTo(expectedReminderList)
+    }
+}
