4d1001f6209b5212021fc67a6e528f4cb7d3f732
Harry S <shorthouse20@gmail.com>
2023-02-01 19:26:32 +0000

    Add listviewmodel, contains all 3 list types together

---
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/ListViewModel.kt
@@ -0,0 +1,41 @@
+package dev.shorthouse.remindme.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.shorthouse.remindme.compose.state.ReminderState
+import dev.shorthouse.remindme.data.ReminderRepository
+import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.utilities.enums.ReminderList
+import dev.shorthouse.remindme.utilities.enums.ReminderSortOrder
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+@HiltViewModel
+class ListViewModel @Inject constructor(
+    private val repository: ReminderRepository,
+) : ViewModel() {
+    fun getReminderStates(
+        reminderList: ReminderList,
+        reminderSortOrder: ReminderSortOrder
+    ): LiveData<List<ReminderState>> {
+        val reminders = when (reminderList) {
+            ReminderList.OVERDUE -> repository.getOverdueReminders()
+            ReminderList.SCHEDULED -> repository.getScheduledReminders()
+            ReminderList.COMPLETED -> repository.getCompletedReminders()
+        }
+
+        return reminders
+            .map { sortReminders(it, reminderSortOrder) }
+            .map { it.map { reminder -> ReminderState(reminder) } }
+            .asLiveData()
+    }
+
+    private fun sortReminders(reminders: List<Reminder>, reminderSortOrder: ReminderSortOrder): List<Reminder> {
+        return when (reminderSortOrder) {
+            ReminderSortOrder.EARLIEST_DATE_FIRST -> reminders.sortedBy { it.startDateTime }
+            else -> reminders.sortedByDescending { it.startDateTime }
+        }
+    }
+}
