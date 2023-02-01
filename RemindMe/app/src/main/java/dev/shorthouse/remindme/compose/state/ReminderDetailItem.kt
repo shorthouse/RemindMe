163e9e498b5bb278df61b7da49f6dbad900a2212
Harry S <shorthouse20@gmail.com>
2023-02-01 19:27:49 +0000

    Remove reminder details, replaced with info in card

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/compose/state/ReminderDetailItem.kt
+++
@@ -1,9 +0,0 @@
-package dev.shorthouse.remindme.compose.state
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class ReminderDetailItem(
-    val icon: ImageVector,
-    val label: String,
-    val contentDescription: String
-)
