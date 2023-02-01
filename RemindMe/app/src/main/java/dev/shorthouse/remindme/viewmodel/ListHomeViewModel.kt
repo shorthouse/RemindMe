1b586c651fa9c260ef5ac2ea0cc2f16bd61531c4
Harry S <shorthouse20@gmail.com>
2023-02-01 19:31:06 +0000

    Add reminder actions logic

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/ListHomeViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/viewmodel/ListHomeViewModel.kt
@@ -1,30 +1,104 @@
 package dev.shorthouse.remindme.viewmodel
 
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import dev.shorthouse.remindme.utilities.enums.ReminderList
-import dev.shorthouse.remindme.utilities.enums.ReminderSortOrder
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.shorthouse.remindme.compose.state.ReminderState
+import dev.shorthouse.remindme.data.ReminderRepository
+import dev.shorthouse.remindme.di.IoDispatcher
+import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.utilities.DAYS_IN_WEEK
+import dev.shorthouse.remindme.utilities.NotificationScheduler
+import dev.shorthouse.remindme.utilities.floor
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
+import javax.inject.Inject
+import kotlin.time.Duration.Companion.days
+import kotlin.time.DurationUnit
+import kotlin.time.times
+import kotlin.time.toDuration
 
-class ListHomeViewModel : ViewModel() {
-    var selectedSortIndex by mutableStateOf(0)
+@HiltViewModel
+class ListHomeViewModel @Inject constructor(
+    private val repository: ReminderRepository,
+    private val notificationScheduler: NotificationScheduler,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) : ViewModel() {
+    var selectedReminderState by mutableStateOf(ReminderState())
 
-    val selectedReminderSortOrder by derivedStateOf {
-        when (selectedSortIndex) {
-            0 -> ReminderSortOrder.EARLIEST_DATE_FIRST
-            else -> ReminderSortOrder.LATEST_DATE_FIRST
+    fun completeOnetimeReminder(reminderState: ReminderState) {
+        val reminder = reminderState.toReminder()
+
+        removeReminderNotification(reminder)
+
+        viewModelScope.launch(ioDispatcher) {
+            repository.completeReminder(reminder.id)
         }
     }
 
-    var selectedNavigateIndex by mutableStateOf(0)
+    fun completeRepeatReminderOccurrence(reminderState: ReminderState) {
+        val reminder = reminderState.toReminder()
 
-    val selectedReminderList by derivedStateOf {
-        when (selectedNavigateIndex) {
-            0 -> ReminderList.OVERDUE
-            1 -> ReminderList.SCHEDULED
-            else -> ReminderList.COMPLETED
+        val updatedReminder = reminder.copy(
+            startDateTime = getUpdatedReminderStartDateTime(reminder)
+        )
+
+        viewModelScope.launch(ioDispatcher) {
+            repository.updateReminder(updatedReminder)
         }
+    }
+
+    fun completeRepeatReminderSeries(reminderState: ReminderState) {
+        val reminder = reminderState.toReminder()
+
+        removeReminderNotification(reminder)
+
+        viewModelScope.launch(ioDispatcher) {
+            repository.completeReminder(reminder.id)
+        }
+    }
+
+    fun deleteReminder(reminderState: ReminderState) {
+        val reminder = reminderState.toReminder()
+
+        removeReminderNotification(reminder)
+
+        viewModelScope.launch(ioDispatcher) {
+            repository.deleteReminder(reminder)
+        }
+    }
+
+    private fun removeReminderNotification(reminder: Reminder) {
+        notificationScheduler.removeDisplayingNotification(reminder.id.toInt())
+
+        if (reminder.isNotificationSent) {
+            notificationScheduler.cancelScheduledReminderNotification(reminder)
+        }
+    }
+
+    private fun getUpdatedReminderStartDateTime(reminder: Reminder): ZonedDateTime {
+        val repeatInterval = reminder.repeatInterval ?: return reminder.startDateTime
+
+        val repeatDuration = when (repeatInterval.unit) {
+            ChronoUnit.DAYS -> repeatInterval.amount.days
+            else -> (repeatInterval.amount * DAYS_IN_WEEK).days
+        }
+
+        val secondsToNewStartDateTime = ZonedDateTime.now()
+            .toEpochSecond()
+            .minus(reminder.startDateTime.toEpochSecond())
+            .toDuration(DurationUnit.SECONDS)
+            .div(repeatDuration)
+            .floor()
+            .inc()
+            .times(repeatDuration)
+            .inWholeSeconds
+
+        return reminder.startDateTime.plusSeconds(secondsToNewStartDateTime)
     }
 }
