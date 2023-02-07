0583e8b369f36a3ff3b328dd955c6adeb921dce9
Harry S <shorthouse20@gmail.com>
2023-02-07 21:17:57 +0000

    Rename to reminder list order

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/util/enums/ReminderListOrder.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/util/enums/ReminderListOrder.kt
@@ -3,7 +3,7 @@ package dev.shorthouse.remindme.util.enums
 import androidx.annotation.StringRes
 import dev.shorthouse.remindme.R
 
-enum class ReminderSortOrder(@StringRes val displayNameStringId: Int) {
+enum class ReminderListOrder(@StringRes val displayNameStringId: Int) {
     EARLIEST_DATE_FIRST(R.string.sort_dialog_option_date_earliest),
     LATEST_DATE_FIRST(R.string.sort_dialog_option_date_latest)
 }
