90005b08390f14992fa6557dddd52a6fc677a84a
Harry S <shorthouse20@gmail.com>
2023-02-18 15:17:05 +0000

    Remove unused enums

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/enums/ReminderBottomSheet.kt
+++
@@ -1,6 +0,0 @@
-package dev.shorthouse.remindme.enums
-
-enum class ReminderBottomSheet {
-    NAVIGATE,
-    REMINDER_ACTIONS
-}
--- RemindMe/app/src/main/java/dev/shorthouse/remindme/enums/ReminderListScreen.kt
+++
@@ -1,7 +0,0 @@
-package dev.shorthouse.remindme.enums
-
-enum class ReminderListScreen {
-    ACTIVE,
-    COMPLETED,
-    SEARCH
-}
