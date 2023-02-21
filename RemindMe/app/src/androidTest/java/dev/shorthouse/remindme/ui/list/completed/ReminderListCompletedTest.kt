25af92373adb631ac260146a0cea646f62623c85
Harry S <shorthouse20@gmail.com>
2023-02-21 21:04:32 +0000

    Add compose UI tests

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/ui/list/completed/ReminderListCompletedTest.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/ui/list/completed/ReminderListCompletedTest.kt
@@ -1,4 +1,4 @@
-package dev.shorthouse.remindme.compose
+package dev.shorthouse.remindme.ui.list.completed
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
