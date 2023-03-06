3828f3e4d8e8cc9f60a14dfe8130d234dcd5f729
Harry S <shorthouse20@gmail.com>
2023-03-06 22:27:33 +0000

    Add m3 typography

---
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/theme/m3/Typography.kt
@@ -0,0 +1,62 @@
+package dev.shorthouse.remindme.ui.theme.m3
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import dev.shorthouse.remindme.R
+import dev.shorthouse.remindme.ui.theme.m2.SubtitleGrey
+
+private val Roboto = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold)
+)
+
+val AppTypography = Typography(
+    headlineSmall = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = Roboto
+    ),
+    titleLarge = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = Roboto
+    ),
+    titleMedium = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Normal,
+        color = SubtitleGrey,
+        fontFamily = Roboto
+    ),
+    titleSmall = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = Roboto
+    ),
+    bodyLarge = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = Roboto
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.25.sp,
+        fontFamily = Roboto
+    ),
+    bodySmall = TextStyle(
+        fontSize = 15.sp,
+        letterSpacing = 0.2.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = Roboto
+    ),
+    labelLarge = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = Roboto
+    )
+)
