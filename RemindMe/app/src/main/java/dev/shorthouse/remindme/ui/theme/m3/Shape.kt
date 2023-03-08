6be12c0ddab7d960fee8f43437732a838a8a3122
Harry S <shorthouse20@gmail.com>
2023-03-08 16:50:40 +0000

    Add extraLarge and extraSmall shapes

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/theme/m3/Shape.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/theme/m3/Shape.kt
@@ -6,9 +6,9 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val AppShapes = Shapes(
-    extraSmall = ShapeDefaults.ExtraSmall,
+    extraSmall = RoundedCornerShape(4.dp),
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(8.dp),
     large = RoundedCornerShape(0.dp),
-    extraLarge = ShapeDefaults.ExtraLarge
+    extraLarge = RoundedCornerShape(0.dp),
 )
