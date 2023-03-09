02c6ea502c10f7a99fd38d86e92eb2375a79ee76
Harry S <shorthouse20@gmail.com>
2023-03-09 20:16:19 +0000

    Migrate from Material 2 to Material 3 in Compose

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/component/dialog/DatePickerDialog.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/component/dialog/DatePickerDialog.kt
@@ -1,15 +1,10 @@
 package dev.shorthouse.remindme.ui.component.dialog
 
 import android.content.res.Configuration
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.MaterialDialogState
-import com.vanpra.composematerialdialogs.datetime.date.datepicker
-import dev.shorthouse.remindme.R
-import dev.shorthouse.remindme.ui.theme.m2.RemindMeTheme
+import dev.shorthouse.remindme.ui.theme.m3.AppTheme
 import java.time.LocalDate
 
 @Composable
@@ -18,33 +13,33 @@ fun DatePickerDialog(
     onDateChange: (LocalDate) -> Unit,
     dialogState: MaterialDialogState
 ) {
-    MaterialDialog(
-        dialogState = dialogState,
-        buttons = {
-            positiveButton(
-                text = stringResource(R.string.dialog_action_ok),
-                textStyle = MaterialTheme.typography.button
-            )
-            negativeButton(
-                text = stringResource(R.string.dialog_action_cancel),
-                textStyle = MaterialTheme.typography.button
-            )
-        }
-    ) {
-        datepicker(
-            initialDate = date,
-            title = "",
-            onDateChange = onDateChange,
-            allowedDateValidator = { it.isAfter(LocalDate.now().minusDays(1)) }
-        )
-    }
+//    MaterialDialog(
+//        dialogState = dialogState,
+//        buttons = {
+//            positiveButton(
+//                text = stringResource(R.string.dialog_action_ok),
+//                textStyle = MaterialTheme.typography.button
+//            )
+//            negativeButton(
+//                text = stringResource(R.string.dialog_action_cancel),
+//                textStyle = MaterialTheme.typography.button
+//            )
+//        }
+//    ) {
+//        datepicker(
+//            initialDate = date,
+//            title = "",
+//            onDateChange = onDateChange,
+//            allowedDateValidator = { it.isAfter(LocalDate.now().minusDays(1)) }
+//        )
+//    }
 }
 
 @Composable
 @Preview(name = "Light Mode")
 @Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 fun DatePickerDialogPreview() {
-    RemindMeTheme {
+    AppTheme {
         val dialogState = MaterialDialogState(initialValue = true)
 
         DatePickerDialog(
