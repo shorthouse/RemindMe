e605f0fd8569d036d74fb49e353e2779a97448ab
Harry S <shorthouse20@gmail.com>
2023-03-11 15:08:37 +0000

    Change size of extralarge to 28.dp

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/theme/m3/Shape.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/theme/m3/Shape.kt
@@ -6,8 +6,8 @@ import androidx.compose.ui.unit.dp
 
 val AppShapes = Shapes(
     extraSmall = RoundedCornerShape(4.dp),
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(8.dp),
-    large = RoundedCornerShape(0.dp),
-    extraLarge = RoundedCornerShape(0.dp)
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(28.dp)
 )
