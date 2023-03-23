e647f760e939d14f6b0214772688c40ee7fba51e
Harry S <shorthouse20@gmail.com>
2023-03-23 22:15:55 +0000

    Add bottom sheet reminder actions preview

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/component/sheet/BottomSheetReminderActions.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/component/sheet/BottomSheetReminderActions.kt
@@ -1,15 +1,16 @@
 package dev.shorthouse.remindme.ui.component.sheet
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.TaskAlt
+import android.content.res.Configuration
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import dev.shorthouse.remindme.R
+import dev.shorthouse.remindme.ui.previewdata.DefaultReminderProvider
 import dev.shorthouse.remindme.ui.state.ReminderState
+import dev.shorthouse.remindme.ui.theme.AppTheme
 import dev.shorthouse.remindme.ui.util.enums.ReminderAction
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -26,7 +27,22 @@ fun BottomSheetReminderActions(
     ) {
         SheetReminderActions(
             reminderState = reminderState,
-            onReminderActionItemSelected = onReminderActionSelected
+            onReminderActionSelected = onReminderActionSelected
+        )
+    }
+}
+
+@Composable
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+fun BottomSheetReminderActionsPreview(
+    @PreviewParameter(DefaultReminderProvider::class) reminderState: ReminderState
+) {
+    AppTheme {
+        BottomSheetReminderActions(
+            reminderState = ReminderState(),
+            onReminderActionSelected = {},
+            onDismissRequest = {}
         )
     }
 }
