5602133df3c9461495ab6ad78fb816a31055fa9e
Harry S <shorthouse20@gmail.com>
2023-03-27 19:12:55 +0100

    Change gradle to kotlin gradle

---
+++ RemindMe/build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
