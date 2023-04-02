1c1b70f9a61b9c38222a01528675d52371d2c971
Harry S <shorthouse20@gmail.com>
2023-04-02 15:48:18 +0100

    Add create reminder model function

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/util/ReminderTestUtil.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/util/ReminderTestUtil.kt
@@ -1,9 +1,32 @@
 package dev.shorthouse.remindme.util
 
+import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.model.RepeatInterval
 import dev.shorthouse.remindme.ui.state.ReminderState
 import java.time.LocalTime
+import java.time.ZonedDateTime
 
 class ReminderTestUtil {
+    fun createReminder(
+        id: Long = 1L,
+        name: String = "Test Reminder",
+        startDateTime: ZonedDateTime = ZonedDateTime.now(),
+        repeatInterval: RepeatInterval? = null,
+        notes: String? = null,
+        isCompleted: Boolean = false,
+        isNotificationSent: Boolean = false
+    ): Reminder {
+        return Reminder(
+            id = id,
+            name = name,
+            startDateTime = startDateTime,
+            repeatInterval = repeatInterval,
+            notes = notes,
+            isCompleted = isCompleted,
+            isNotificationSent = isNotificationSent
+        )
+    }
+
     fun createReminderState(
         id: Long = 1,
         name: String = "Reminder name",
