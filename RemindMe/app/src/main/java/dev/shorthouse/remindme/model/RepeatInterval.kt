7ad52a175ab7e673ff976bdb96d04a6087119c5f
Harry S <shorthouse20@gmail.com>
2023-04-02 15:48:25 +0100

    Remove unneeded import

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/model/RepeatInterval.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/model/RepeatInterval.kt
@@ -1,16 +1,8 @@
 package dev.shorthouse.remindme.model
 
-import dev.shorthouse.remindme.R
 import java.time.temporal.ChronoUnit
 
 data class RepeatInterval(
     val amount: Long,
     val unit: ChronoUnit
-) {
-    fun getPluralId(): Int {
-        return when (unit) {
-            ChronoUnit.DAYS -> R.plurals.repeat_interval_days
-            else -> R.plurals.repeat_interval_days
-        }
-    }
-}
+)
