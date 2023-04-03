1456bc0b5a2dce9cf53ba9132065e9189c0ad7e8
Harry S <shorthouse20@gmail.com>
2023-04-03 21:44:31 +0100

    Remove unused createReminderState() helper function

--- RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/util/ReminderTestUtil.kt
+++ RemindMe/app/src/androidTest/java/dev/shorthouse/remindme/util/ReminderTestUtil.kt
@@ -2,8 +2,6 @@ package dev.shorthouse.remindme.util
 
 import dev.shorthouse.remindme.model.Reminder
 import dev.shorthouse.remindme.model.RepeatInterval
-import dev.shorthouse.remindme.ui.state.ReminderState
-import java.time.LocalTime
 import java.time.ZonedDateTime
 
 class ReminderTestUtil {
@@ -24,32 +22,6 @@ class ReminderTestUtil {
             notes = notes,
             isCompleted = isCompleted,
             isNotificationSent = isNotificationSent
-        )
-    }
-
-    fun createReminderState(
-        id: Long = 1,
-        name: String = "Reminder name",
-        date: String = "Sat, 01 Jan 2000",
-        time: LocalTime = LocalTime.parse("08:30"),
-        isNotificationSent: Boolean = false,
-        isRepeatReminder: Boolean = false,
-        repeatAmount: String = "1",
-        repeatUnit: String = "Day",
-        notes: String? = null,
-        isCompleted: Boolean = false
-    ): ReminderState {
-        return ReminderState(
-            id = id,
-            name = name,
-            date = date,
-            time = time,
-            isNotificationSent = isNotificationSent,
-            isRepeatReminder = isRepeatReminder,
-            repeatAmount = repeatAmount,
-            repeatUnit = repeatUnit,
-            notes = notes,
-            isCompleted = isCompleted
         )
     }
 }
