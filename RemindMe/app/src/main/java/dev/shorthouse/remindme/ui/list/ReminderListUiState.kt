216fc5fc2ae9de42c62ce96fd39e00733297a92b
Harry S <shorthouse20@gmail.com>
2023-04-25 19:52:03 +0100

    Extract out ui state into separate class

---
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/list/ReminderListUiState.kt
@@ -0,0 +1,14 @@
+package dev.shorthouse.remindme.ui.list
+
+import dev.shorthouse.remindme.data.protodatastore.ReminderFilter
+import dev.shorthouse.remindme.data.protodatastore.ReminderSort
+import dev.shorthouse.remindme.model.Reminder
+
+data class ReminderListUiState(
+    val reminders: List<Reminder> = emptyList(),
+    val reminderFilter: ReminderFilter = ReminderFilter.UPCOMING,
+    val reminderSortOrder: ReminderSort = ReminderSort.BY_EARLIEST_DATE_FIRST,
+    val searchQuery: String = "",
+    val isSearchBarShown: Boolean = false,
+    val isLoading: Boolean = true
+)
