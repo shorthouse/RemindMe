44ffe31975cb18b75c050b048e38fc2f522da369
Harry S <shorthouse20@gmail.com>
2023-04-26 22:16:52 +0100

    Continue building on reminder details screen and viewmodel

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/details/ReminderDetailsViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/details/ReminderDetailsViewModel.kt
@@ -5,15 +5,15 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shorthouse.remindme.data.source.local.ReminderRepository
 import dev.shorthouse.remindme.di.IoDispatcher
+import dev.shorthouse.remindme.domain.reminder.CompleteOnetimeReminderUseCase
+import dev.shorthouse.remindme.domain.reminder.CompleteRepeatReminderSeriesUseCase
+import dev.shorthouse.remindme.domain.reminder.DeleteReminderUseCase
 import dev.shorthouse.remindme.domain.reminder.UpdateReminderUseCase
 import dev.shorthouse.remindme.model.Reminder
-import dev.shorthouse.remindme.ui.state.ReminderState
-import java.time.ZonedDateTime
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -21,42 +21,52 @@ import kotlinx.coroutines.launch
 class ReminderDetailsViewModel @Inject constructor(
     private val reminderRepository: ReminderRepository,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
-    private val updateReminderUseCase: UpdateReminderUseCase
+    private val updateReminderUseCase: UpdateReminderUseCase,
+    private val deleteReminderUseCase: DeleteReminderUseCase,
+    private val completeOnetimeReminderUseCase: CompleteOnetimeReminderUseCase,
+    private val completeRepeatReminderSeriesUseCase: CompleteRepeatReminderSeriesUseCase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(ReminderDetailsUiState())
 
     val uiState: StateFlow<ReminderDetailsUiState>
         get() = _uiState
 
-    val reminder = MutableStateFlow(
-        ReminderState().toReminder()
-    )
-
     fun setReminder(reminderId: Long) {
         _uiState.update { it.copy(isLoading = true) }
 
         viewModelScope.launch(ioDispatcher) {
-            reminderRepository.getReminder(reminderId)
-                .map {
-                    ReminderDetailsUiState(
-                        initialReminder = it,
-                        isLoading = false
-                    )
-                }.collect {
-                    _uiState.value = it
-                }
+            val initialReminder = reminderRepository.getReminderOneShot(reminderId)
+
+            _uiState.update {
+                it.copy(
+                    initialReminder = initialReminder,
+                    isLoading = false
+                )
+            }
         }
     }
 
-    fun saveReminder(reminderState: ReminderState) {
-        val reminder = reminderState.toReminder()
-
-        if (isReminderValid(reminder)) {
-            updateReminderUseCase(reminder)
+    fun handleEvent(event: ReminderDetailsEvent) {
+        when (event) {
+            is ReminderDetailsEvent.CompleteReminder -> handleCompleteReminder(event.reminder)
+            is ReminderDetailsEvent.DeleteReminder -> handleDeleteReminder(event.reminder)
+            is ReminderDetailsEvent.SaveReminder -> handleSaveReminder(event.reminder)
         }
     }
 
-    private fun isReminderValid(reminder: Reminder): Boolean {
-        return reminder.name.isNotBlank() && reminder.startDateTime.isAfter(ZonedDateTime.now())
+    private fun handleCompleteReminder(reminder: Reminder) {
+        if (reminder.isRepeatReminder) {
+            completeRepeatReminderSeriesUseCase(reminder)
+        } else {
+            completeOnetimeReminderUseCase(reminder)
+        }
+    }
+
+    private fun handleDeleteReminder(reminder: Reminder) {
+        deleteReminderUseCase(reminder)
+    }
+
+    private fun handleSaveReminder(reminder: Reminder) {
+        updateReminderUseCase(reminder)
     }
 }
