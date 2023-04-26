4b5d44f4d2fca099cc32fc919437bb16fe29638c
Harry S <shorthouse20@gmail.com>
2023-04-26 22:16:21 +0100

    Add SaveReminder event

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/details/ReminderDetailsEvent.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/details/ReminderDetailsEvent.kt
@@ -1,4 +1,9 @@
 package dev.shorthouse.remindme.ui.details
 
-class ReminderDetailsEvent {
+import dev.shorthouse.remindme.model.Reminder
+
+sealed interface ReminderDetailsEvent {
+    data class CompleteReminder(val reminder: Reminder) : ReminderDetailsEvent
+    data class DeleteReminder(val reminder: Reminder) : ReminderDetailsEvent
+    data class SaveReminder(val reminder: Reminder) : ReminderDetailsEvent
 }
