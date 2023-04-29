69d28e6e79de8078a9667bdca5f00c269db20fe1
Harry S <shorthouse20@gmail.com>
2023-04-29 12:13:24 +0100

    Add string chronounit mapper

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/util/Extensions.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/util/Extensions.kt
@@ -1,3 +1,13 @@
 package dev.shorthouse.remindme.util
 
+import java.time.temporal.ChronoUnit
+
 fun Double.floor(): Double = kotlin.math.floor(this)
+
+fun String.toChronoUnit(): ChronoUnit? {
+    return when {
+        ChronoUnit.DAYS.name.contains(this, ignoreCase = true) -> ChronoUnit.DAYS
+        ChronoUnit.WEEKS.name.contains(this, ignoreCase = true) -> ChronoUnit.WEEKS
+        else -> null
+    }
+}
