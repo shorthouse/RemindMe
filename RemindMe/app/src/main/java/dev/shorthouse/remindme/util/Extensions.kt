1fbefad7d4f715fe03d1432c7e79c0250edd969a
Harry S <shorthouse20@gmail.com>
2023-04-29 15:17:57 +0100

    Add workaround for disabling bottom sheet gestures

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/util/Extensions.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/util/Extensions.kt
@@ -1,13 +1,27 @@
 package dev.shorthouse.remindme.util
 
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
+import kotlin.math.abs
 
 fun Double.floor(): Double = kotlin.math.floor(this)
 
-fun String.toChronoUnit(): ChronoUnit? {
-    return when {
-        ChronoUnit.DAYS.name.contains(this, ignoreCase = true) -> ChronoUnit.DAYS
-        ChronoUnit.WEEKS.name.contains(this, ignoreCase = true) -> ChronoUnit.WEEKS
-        else -> null
+fun ZonedDateTime.nextHour(): ZonedDateTime = this.truncatedTo(ChronoUnit.HOURS).plusHours(1)
+
+// Workaround for https://issuetracker.google.com/issues/186669832
+fun Modifier.disableBottomSheetSwipe() = pointerInput(Unit) {
+    awaitPointerEventScope {
+        while (true) {
+            awaitPointerEvent(pass = PointerEventPass.Initial).changes.forEach {
+                val offset = it.positionChange()
+                if (abs(offset.y) > 0f) {
+                    it.consume()
+                }
+            }
+        }
     }
 }
