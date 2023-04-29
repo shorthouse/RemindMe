12e20b88634af1251b250e0a99e0e891766e7dd9
Harry S <shorthouse20@gmail.com>
2023-04-29 12:16:02 +0100

    Add updating reminder events

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/details/ReminderDetailsEvent.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/details/ReminderDetailsEvent.kt
@@ -1,9 +1,18 @@
 package dev.shorthouse.remindme.ui.details
 
 import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.model.RepeatInterval
+import java.time.LocalDate
+import java.time.LocalTime
 
 sealed interface ReminderDetailsEvent {
     data class CompleteReminder(val reminder: Reminder) : ReminderDetailsEvent
     data class DeleteReminder(val reminder: Reminder) : ReminderDetailsEvent
     data class SaveReminder(val reminder: Reminder) : ReminderDetailsEvent
+    data class UpdateName(val name: String) : ReminderDetailsEvent
+    data class UpdateDate(val date: LocalDate) : ReminderDetailsEvent
+    data class UpdateTime(val time: LocalTime) : ReminderDetailsEvent
+    data class UpdateNotification(val isNotificationSent: Boolean) : ReminderDetailsEvent
+    data class UpdateRepeatInterval(val repeatInterval: RepeatInterval?) : ReminderDetailsEvent
+    data class UpdateNotes(val notes: String) : ReminderDetailsEvent
 }
