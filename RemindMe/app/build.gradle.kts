747108c355163c24e1f7c67eb802d62dc3e560a0
Harry S <shorthouse20@gmail.com>
2023-05-02 22:20:02 +0100

    Update version number and code

--- RemindMe/app/build.gradle.kts
+++ RemindMe/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "dev.shorthouse.remindme"
         minSdk = 26
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 17
+        versionName = "2.1.4"
 
         testInstrumentationRunner = "dev.shorthouse.remindme.runner.CustomTestRunner"
     }
