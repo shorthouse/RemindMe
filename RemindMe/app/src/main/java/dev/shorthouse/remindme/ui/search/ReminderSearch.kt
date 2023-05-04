40a7f5cd80ca8f48224368534592d20bf966e68c
Harry S (Macbook) <shorthouse20@gmail.com>
2023-05-04 22:30:37 +0100

    Finish wiring up search screen navigation logic

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/search/ReminderSearch.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/search/ReminderSearch.kt
@@ -1,23 +1,22 @@
 package dev.shorthouse.remindme.ui.search
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import dev.shorthouse.remindme.R
-import dev.shorthouse.remindme.ui.component.topappbar.RemindMeTopAppBar
+import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.ui.component.emptystate.EmptyStateSearchReminders
+import dev.shorthouse.remindme.ui.component.searchbar.RemindMeSearchBar
+import dev.shorthouse.remindme.ui.destinations.ReminderDetailsScreenDestination
+import dev.shorthouse.remindme.ui.list.ReminderList
 
 @Composable
 @Destination
@@ -29,7 +28,13 @@ fun ReminderSearchScreen(
 
     ReminderSearchScreen(
         uiState = uiState,
-        onNavigateUp = { navigator.navigateUp() }
+        onNavigateUp = { navigator.navigateUp() },
+        onSearchQueryChange = { viewModel.updateSearchQuery(it) },
+        onNavigateDetails = {
+            navigator.popBackStack()
+            navigator.navigate(ReminderDetailsScreenDestination(it.id))
+        },
+        onCompleteReminder = { viewModel.completeReminder(it) }
     )
 }
 
@@ -37,17 +42,26 @@ fun ReminderSearchScreen(
 fun ReminderSearchScreen(
     uiState: ReminderSearchUiState,
     onNavigateUp: () -> Unit,
-    modifier: Modifier = Modifier
+    onSearchQueryChange: (String) -> Unit,
+    onNavigateDetails: (Reminder) -> Unit,
+    onCompleteReminder: (Reminder) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Scaffold(
         topBar = {
             ReminderSearchTopBar(
+                searchQuery = uiState.searchQuery,
+                onSearchQueryChange = onSearchQueryChange,
                 onNavigateUp = onNavigateUp
             )
         },
         content = { scaffoldPadding ->
             if (!uiState.isLoading) {
                 ReminderSearchContent(
+                    reminders = uiState.searchReminders,
+                    searchQuery = uiState.searchQuery,
+                    onNavigateDetails = onNavigateDetails,
+                    onCompleteReminder = onCompleteReminder,
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(scaffoldPadding)
@@ -59,25 +73,36 @@ fun ReminderSearchScreen(
 }
 
 @Composable
-fun ReminderSearchTopBar(
-    onNavigateUp: () -> Unit,
-    modifier: Modifier = Modifier
+private fun ReminderSearchTopBar(
+    searchQuery: String,
+    onSearchQueryChange: (String) -> Unit,
+    onNavigateUp: () -> Unit
 ) {
-    RemindMeTopAppBar(
-        title = stringResource(R.string.top_bar_title_reminder_search),
-        navigationIcon = {
-            IconButton(onClick = { onNavigateUp() }) {
-                Icon(
-                    imageVector = Icons.Rounded.ArrowBack,
-                    contentDescription = stringResource(R.string.cd_top_app_bar_back)
-                )
-            }
-        },
-        modifier = modifier
+    RemindMeSearchBar(
+        searchQuery = searchQuery,
+        onSearchQueryChange = onSearchQueryChange,
+        onCloseSearch = onNavigateUp
     )
 }
 
 @Composable
-fun ReminderSearchContent(modifier: Modifier) {
-    Text("Reminder search content!")
+fun ReminderSearchContent(
+    reminders: List<Reminder>,
+    searchQuery: String,
+    onNavigateDetails: (Reminder) -> Unit,
+    onCompleteReminder: (Reminder) -> Unit,
+    modifier: Modifier
+) {
+    ReminderList(
+        reminders = reminders,
+        emptyState = {
+            if (searchQuery.isNotEmpty()) {
+                EmptyStateSearchReminders(modifier = modifier)
+            }
+        },
+        onReminderCard = onNavigateDetails,
+        onCompleteReminder = onCompleteReminder,
+        contentPadding = PaddingValues(top = 8.dp, bottom = 8.dp),
+        modifier = modifier
+    )
 }
