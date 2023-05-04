e02d5acee9c58f4d0a39ff31ad197e4fe1639546
Harry S (Macbook) <shorthouse20@gmail.com>
2023-05-04 22:31:31 +0100

    Add flow combine logic for updating ui state

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/search/ReminderSearchViewModel.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/search/ReminderSearchViewModel.kt
@@ -1,18 +1,65 @@
 package dev.shorthouse.remindme.ui.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.shorthouse.remindme.data.source.local.ReminderRepository
 import dev.shorthouse.remindme.di.IoDispatcher
+import dev.shorthouse.remindme.domain.reminder.CompleteReminderUseCase
+import dev.shorthouse.remindme.model.Reminder
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 
 @HiltViewModel
 class ReminderSearchViewModel @Inject constructor(
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+    private val reminderRepository: ReminderRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val completeReminderUseCase: CompleteReminderUseCase,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(ReminderSearchUiState())
 
     val uiState = _uiState.asStateFlow()
+
+    private val _searchQuery = MutableStateFlow("")
+
+    init {
+        initialiseUiState()
+    }
+
+    private fun initialiseUiState() {
+        val remindersFlow = reminderRepository.getReminders()
+        val searchQueryFlow = _searchQuery.asStateFlow()
+
+        combine(remindersFlow, searchQueryFlow) { reminders, searchQuery ->
+            val searchReminders = when {
+                searchQuery.isEmpty() -> emptyList()
+                else -> reminders.filter { it.name.contains(searchQuery, ignoreCase = true) }
+            }
+
+            _uiState.value.copy(
+                searchReminders = searchReminders,
+                isLoading = false
+            )
+        }
+            .flowOn(ioDispatcher)
+            .onEach { _uiState.value = it }
+            .launchIn(viewModelScope)
+    }
+
+    fun updateSearchQuery(query: String) {
+        _uiState.update { it.copy(searchQuery = query) }
+        _searchQuery.value = query
+    }
+
+    fun completeReminder(reminder: Reminder) {
+        completeReminderUseCase(reminder)
+    }
 }
