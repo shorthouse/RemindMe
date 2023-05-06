005a4ce901cfe46bd1629fa22b300e719c7dbc1e
Harry S (Macbook) <shorthouse20@gmail.com>
2023-05-06 11:35:18 +0100

    Add get user prefs flow usecase

---
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/domain/GetUserPreferencesFlowUseCase.kt
@@ -0,0 +1,23 @@
+package dev.shorthouse.remindme.domain
+
+import dev.shorthouse.remindme.data.protodatastore.UserPreferences
+import dev.shorthouse.remindme.data.protodatastore.UserPreferencesRepository
+import dev.shorthouse.remindme.di.IoDispatcher
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+
+class GetUserPreferencesFlowUseCase @Inject constructor(
+    private val userPreferencesRepository: UserPreferencesRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) {
+    operator fun invoke(): Flow<UserPreferences> {
+        return getUserPreferencesFlow()
+    }
+
+    private fun getUserPreferencesFlow(): Flow<UserPreferences> {
+        return userPreferencesRepository.userPreferencesFlow
+            .flowOn(ioDispatcher)
+    }
+}
