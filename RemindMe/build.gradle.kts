fa8afe0dcf21b1f6f0f9a00f2aae6eef7da5f37e
Harry S <shorthouse20@gmail.com>
2023-05-10 17:43:49 +0100

    Update dependencies

--- RemindMe/build.gradle.kts
+++ RemindMe/build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
         classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.5")
         classpath("com.google.firebase:perf-plugin:1.4.2")
     }
 }
