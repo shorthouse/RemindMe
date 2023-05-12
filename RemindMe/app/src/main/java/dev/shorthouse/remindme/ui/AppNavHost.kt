faa4c0cfa91f4fa0bd1f8111fdb3ea82b4423aeb
Harry S (Macbook) <shorthouse20@gmail.com>
2023-05-12 22:58:37 +0100

    Add transition animations

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/AppNavHost.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/AppNavHost.kt
@@ -1,9 +1,30 @@
 package dev.shorthouse.remindme.ui
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 
+@OptIn(ExperimentalMaterialNavigationApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun AppNavHost() {
-    DestinationsNavHost(navGraph = NavGraphs.root)
+    val navHostEngine = rememberAnimatedNavHostEngine(
+        navHostContentAlignment = Alignment.TopCenter,
+        rootDefaultAnimations = RootNavGraphDefaultAnimations(
+            enterTransition = { slideInHorizontally(initialOffsetX = { it }) },
+            exitTransition = { slideOutHorizontally(targetOffsetX = { -it }) },
+            popEnterTransition = { slideInHorizontally(initialOffsetX = { -it }) },
+            popExitTransition = { slideOutHorizontally(targetOffsetX = { it }) }
+        )
+    )
+
+    DestinationsNavHost(
+        navGraph = NavGraphs.root,
+        engine = navHostEngine
+    )
 }
