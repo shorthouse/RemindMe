336bb80b4fadb66a9b2a238822d55c80d3b24a0e
Harry <shorthouse20@gmail.com>
2023-05-23 19:58:14 +0100

    Refactor boot completed from service to work manager work

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/BootCompletedReceiver.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/BootCompletedReceiver.kt
@@ -3,13 +3,31 @@ package dev.shorthouse.remindme.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import dev.shorthouse.remindme.service.RescheduleNotificationsOnRebootService
+import androidx.work.BackoffPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import dev.shorthouse.remindme.worker.RescheduleNotificationsWorker
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+private const val BACKOFF_DELAY_MILLIS = 10000L
 
 class BootCompletedReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var workManager: WorkManager
+
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == "android.intent.action.BOOT_COMPLETED" && context != null) {
-            val serviceIntent = Intent(context, RescheduleNotificationsOnRebootService::class.java)
-            context.startForegroundService(serviceIntent)
+            val workRequest = OneTimeWorkRequestBuilder<RescheduleNotificationsWorker>()
+                .setBackoffCriteria(
+                    BackoffPolicy.LINEAR,
+                    BACKOFF_DELAY_MILLIS,
+                    TimeUnit.MILLISECONDS
+                )
+                .build()
+
+            workManager.enqueue(workRequest)
         }
     }
 }
