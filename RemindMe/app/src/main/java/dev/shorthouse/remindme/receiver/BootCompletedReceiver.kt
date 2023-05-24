01e9601862fbb6ffee4265b178a25639c448e2db
Harry S <shorthouse20@gmail.com>
2023-05-24 22:19:33 +0100

    Add @AndroidEntryPoint in broadcast receiver

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/BootCompletedReceiver.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/BootCompletedReceiver.kt
@@ -3,15 +3,18 @@ package dev.shorthouse.remindme.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.work.BackoffPolicy
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import dev.shorthouse.remindme.worker.RescheduleNotificationsWorker
+import dagger.hilt.android.AndroidEntryPoint
+import dev.shorthouse.remindme.worker.RescheduleReminderNotificationsWorker
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 private const val BACKOFF_DELAY_MILLIS = 10000L
 
+@AndroidEntryPoint
 class BootCompletedReceiver : BroadcastReceiver() {
 
     @Inject
@@ -19,7 +22,7 @@ class BootCompletedReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == "android.intent.action.BOOT_COMPLETED" && context != null) {
-            val workRequest = OneTimeWorkRequestBuilder<RescheduleNotificationsWorker>()
+            val workRequest = OneTimeWorkRequestBuilder<RescheduleReminderNotificationsWorker>()
                 .setBackoffCriteria(
                     BackoffPolicy.LINEAR,
                     BACKOFF_DELAY_MILLIS,
@@ -28,6 +31,7 @@ class BootCompletedReceiver : BroadcastReceiver() {
                 .build()
 
             workManager.enqueue(workRequest)
+            Log.d("HDS", "work queued")
         }
     }
 }
