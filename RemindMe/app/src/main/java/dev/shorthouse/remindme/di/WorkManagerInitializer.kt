6df42376508f6bf4502d3787fea058c5fd5affce
Harry S (Macbook) <shorthouse20@gmail.com>
2023-05-25 18:12:01 +0100

    Implement fix for worker calling through WorkerManagerInitializer

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/di/WorkManagerInitializer.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/di/WorkManagerInitializer.kt
@@ -1,29 +1,45 @@
 package dev.shorthouse.remindme.di
 
 import android.content.Context
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.startup.Initializer
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class WorkManagerInitializer : Initializer<WorkManager> {
-
+object WorkManagerInitializer : Initializer<WorkManager> {
     @Provides
     @Singleton
     override fun create(@ApplicationContext context: Context): WorkManager {
-        val configuration = Configuration.Builder().build()
+        val entryPoint = EntryPointAccessors.fromApplication(
+            context,
+            WorkManagerInitializerEntryPoint::class.java
+        )
+        val configuration = Configuration
+            .Builder()
+            .setWorkerFactory(entryPoint.hiltWorkerFactory())
+            .build()
+
         WorkManager.initialize(context, configuration)
         return WorkManager.getInstance(context)
     }
 
-    override fun dependencies(): List<Class<out Initializer<*>>> {
-        return emptyList()
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+
+    @InstallIn(SingletonComponent::class)
+    @EntryPoint
+    interface WorkManagerInitializerEntryPoint {
+        fun hiltWorkerFactory(): HiltWorkerFactory
     }
 }
