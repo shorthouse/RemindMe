4ad8609d89f81c21517710fd8a5cc6de73a45bdf
Harry S (Macbook) <shorthouse20@gmail.com>
2023-05-25 18:12:57 +0100

    Change intent action from string to enum

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/TimeZoneChangedReceiver.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/TimeZoneChangedReceiver.kt
@@ -7,6 +7,7 @@ import androidx.work.BackoffPolicy
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
+import dagger.hilt.android.AndroidEntryPoint
 import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.worker.UpdateRemindersTimeZoneWorker
 import java.util.concurrent.TimeUnit
@@ -14,29 +15,30 @@ import javax.inject.Inject
 
 private const val BACKOFF_DELAY_MILLIS = 10000L
 
+@AndroidEntryPoint
 class TimeZoneChangedReceiver : BroadcastReceiver() {
 
     @Inject
     lateinit var workManager: WorkManager
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == "android.intent.action.TIMEZONE_CHANGED" && context != null) {
-            val newTimeZone = intent.getStringExtra(context.getString(R.string.key_time_zone))
+        if (intent?.action != Intent.ACTION_TIMEZONE_CHANGED || context == null) return
 
-            val workRequest = OneTimeWorkRequestBuilder<UpdateRemindersTimeZoneWorker>()
-                .setBackoffCriteria(
-                    BackoffPolicy.LINEAR,
-                    BACKOFF_DELAY_MILLIS,
-                    TimeUnit.MILLISECONDS
-                )
-                .setInputData(
-                    Data.Builder()
-                        .putString(context.getString(R.string.key_time_zone), newTimeZone)
-                        .build()
-                )
-                .build()
+        val newTimeZone = intent.getStringExtra(context.getString(R.string.key_time_zone))
 
-            workManager.enqueue(workRequest)
-        }
+        val workRequest = OneTimeWorkRequestBuilder<UpdateRemindersTimeZoneWorker>()
+            .setBackoffCriteria(
+                BackoffPolicy.LINEAR,
+                BACKOFF_DELAY_MILLIS,
+                TimeUnit.MILLISECONDS
+            )
+            .setInputData(
+                Data.Builder()
+                    .putString(context.getString(R.string.key_time_zone), newTimeZone)
+                    .build()
+            )
+            .build()
+
+        workManager.enqueue(workRequest)
     }
 }
