0ca881bb619402ced496af477ad9c6f7f7d43524
Harry S (Macbook) <shorthouse20@gmail.com>
2023-05-25 18:12:45 +0100

    Change intent flag from string to enum

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/BootCompletedReceiver.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/BootCompletedReceiver.kt
@@ -3,7 +3,6 @@ package dev.shorthouse.remindme.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.work.BackoffPolicy
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
@@ -21,17 +20,16 @@ class BootCompletedReceiver : BroadcastReceiver() {
     lateinit var workManager: WorkManager
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == "android.intent.action.BOOT_COMPLETED" && context != null) {
-            val workRequest = OneTimeWorkRequestBuilder<RescheduleReminderNotificationsWorker>()
-                .setBackoffCriteria(
-                    BackoffPolicy.LINEAR,
-                    BACKOFF_DELAY_MILLIS,
-                    TimeUnit.MILLISECONDS
-                )
-                .build()
+        if (intent?.action != Intent.ACTION_BOOT_COMPLETED || context == null) return
 
-            workManager.enqueue(workRequest)
-            Log.d("HDS", "work queued")
-        }
+        val workRequest = OneTimeWorkRequestBuilder<RescheduleReminderNotificationsWorker>()
+            .setBackoffCriteria(
+                BackoffPolicy.LINEAR,
+                BACKOFF_DELAY_MILLIS,
+                TimeUnit.MILLISECONDS
+            )
+            .build()
+
+        workManager.enqueue(workRequest)
     }
 }
