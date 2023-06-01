ba13b698c147a084fba77ff0fcf5880475de5677
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-01 22:09:33 +0100

    Add snackbar error support to reminder details

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/screen/addedit/ReminderAddEditUiState.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/ui/screen/addedit/ReminderAddEditUiState.kt
@@ -1,10 +1,12 @@
-package dev.shorthouse.remindme.ui.addedit
+package dev.shorthouse.remindme.ui.screen.addedit
 
 import dev.shorthouse.remindme.model.Reminder
+import dev.shorthouse.remindme.util.SnackbarMessage
 
 data class ReminderAddEditUiState(
     val reminder: Reminder = Reminder(),
     val initialReminder: Reminder = Reminder(),
     val isReminderValid: Boolean = false,
+    val snackbarMessage: SnackbarMessage? = null,
     val isLoading: Boolean = false
 )
