b18fd7841002d79a05a6d72e4583387a043a8e66
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-01 22:08:47 +0100

    Update getReminderOneShot() to use result class

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/domain/reminder/GetReminderUseCase.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/domain/reminder/GetReminderUseCase.kt
@@ -1,5 +1,6 @@
 package dev.shorthouse.remindme.domain.reminder
 
+import dev.shorthouse.remindme.data.Result
 import dev.shorthouse.remindme.data.source.local.ReminderRepository
 import dev.shorthouse.remindme.di.IoDispatcher
 import dev.shorthouse.remindme.model.Reminder
@@ -11,11 +12,11 @@ class GetReminderUseCase @Inject constructor(
     private val reminderRepository: ReminderRepository,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(reminderId: Long): Reminder {
+    suspend operator fun invoke(reminderId: Long): Result<Reminder> {
         return getReminder(reminderId = reminderId)
     }
 
-    private suspend fun getReminder(reminderId: Long): Reminder {
+    private suspend fun getReminder(reminderId: Long): Result<Reminder> {
         return withContext(ioDispatcher) {
             reminderRepository.getReminderOneShot(reminderId)
         }
