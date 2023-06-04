6cb94fc47fa897ac1f8de447f039fab24ebd44d0
Harry S <shorthouse20@gmail.com>
2023-06-04 21:52:36 +0100

    Refactor to inject coroutine dispatcher

--- RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/NotificationActionDoneReceiver.kt
+++ RemindMe/app/src/main/java/dev/shorthouse/remindme/receiver/NotificationActionDoneReceiver.kt
@@ -6,15 +6,20 @@ import android.content.Intent
 import dagger.hilt.android.AndroidEntryPoint
 import dev.shorthouse.remindme.R
 import dev.shorthouse.remindme.data.Result
+import dev.shorthouse.remindme.di.IoDispatcher
 import dev.shorthouse.remindme.domain.reminder.CompleteReminderUseCase
 import dev.shorthouse.remindme.domain.reminder.GetReminderUseCase
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class NotificationActionDoneReceiver : BroadcastReceiver() {
+
+    @Inject
+    @IoDispatcher
+    lateinit var ioDispatcher: CoroutineDispatcher
 
     @Inject
     lateinit var getReminderUseCase: GetReminderUseCase
@@ -30,7 +35,7 @@ class NotificationActionDoneReceiver : BroadcastReceiver() {
 
         if (reminderId == null || reminderId == -1L) return
 
-        CoroutineScope(SupervisorJob()).launch {
+        CoroutineScope(ioDispatcher).launch {
             val result = getReminderUseCase(reminderId)
 
             if (result is Result.Success) {
